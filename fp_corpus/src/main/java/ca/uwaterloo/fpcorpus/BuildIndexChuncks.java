39130f703f6826dc66bd9a456a0749fe907f365c
Younos Aboulnaga <yaboulna@uwaterloo.ca>
2012-07-17 13:53:42 -0400

    q

--- fp_corpus/src/main/java/ca/uwaterloo/fpcorpus/BuildIndexChuncks.java
+++ fp_corpus/src/main/java/ca/uwaterloo/fpcorpus/BuildIndexChuncks.java
@@ -23,61 +23,61 @@ import org.slf4j.LoggerFactory;
 import ca.uwaterloo.twitter.ItemSetIndexBuilder;
 
 public class BuildIndexChuncks {
-	private static final Logger LOG = LoggerFactory
-			.getLogger(BuildIndexChuncks.class);
-
-	/**
-	 * @param args
-	 * @throws ParseException
-	 * @throws IOException
-	 * @throws NoSuchAlgorithmException
-	 * @throws LockObtainFailedException
-	 * @throws CorruptIndexException
-	 */
-	public static void main(String[] args) throws CorruptIndexException,
-			LockObtainFailedException, NoSuchAlgorithmException, IOException,
-			ParseException {
-
-		File inDir = new File(args[0]);
-		File outDir = new File(args[1]);
-		int days = Integer.parseInt(args[2]);
-
-		for (int i = 1; i < days; ++i) {
-			final int dayNum = i;
-			File dayOut = new File(outDir, "d" + i);
-			for (File dayIn : inDir.listFiles(new FilenameFilter() {
-
-				public boolean accept(File dir, String name) {
-					return name.endsWith("_day" + dayNum);
-				}
-			})) {
-
-				for (File intervalStartIn : dayIn.listFiles()) {
-					File intervalOut = new File(dayOut,
-							intervalStartIn.getName());
-
-					File inervalEndIn = intervalStartIn.listFiles()[0];
-					intervalOut = new File(intervalOut, inervalEndIn.getName());
-
-					Path freqPattsPath = new Path(inervalEndIn.toURI()
-							.toString() + "/" + PFPGrowth.FREQUENT_PATTERNS);
-					LOG.info("Indexing " + freqPattsPath);
-					ItemSetIndexBuilder builder = new ItemSetIndexBuilder();
-					Map<String, SummaryStatistics> stats = builder.buildIndex(
-							freqPattsPath, new File(intervalOut, "index"),
-							Long.parseLong(intervalStartIn.getName()),
-							Long.parseLong(inervalEndIn.getName()), null);
-
-					for (String key : stats.keySet()) {
-						FileUtils.writeStringToFile(new File(new File(
-								intervalOut, "stats"), key), stats.get(key)
-								.toString());
-					}
-				}
-
-			}
-		}
-
-	}
-
+  private static final Logger LOG = LoggerFactory
+      .getLogger(BuildIndexChuncks.class);
+  
+  /**
+   * @param args
+   * @throws ParseException
+   * @throws IOException
+   * @throws NoSuchAlgorithmException
+   * @throws LockObtainFailedException
+   * @throws CorruptIndexException
+   */
+  public static void main(String[] args) throws CorruptIndexException,
+      LockObtainFailedException, NoSuchAlgorithmException, IOException,
+      ParseException {
+    
+    File inDir = new File(args[0]);
+    File outDir = new File(args[1]);
+    int days = Integer.parseInt(args[2]);
+    
+    for (int i = 1; i <= days; ++i) {
+      final int dayNum = i;
+      File dayOut = new File(outDir, "d" + i);
+      for (File dayIn : inDir.listFiles(new FilenameFilter() {
+        
+        public boolean accept(File dir, String name) {
+          return name.endsWith("_day" + dayNum);
+        }
+      })) {
+        
+        for (File intervalStartIn : dayIn.listFiles()) {
+          File intervalOut = new File(dayOut,
+              intervalStartIn.getName());
+          
+          File inervalEndIn = intervalStartIn.listFiles()[0];
+          intervalOut = new File(intervalOut, inervalEndIn.getName());
+          
+          Path freqPattsPath = new Path(inervalEndIn.toURI()
+              .toString() + "/" + PFPGrowth.FREQUENT_PATTERNS);
+          LOG.info("Indexing {} to {}", freqPattsPath, intervalOut.getAbsolutePath());
+          ItemSetIndexBuilder builder = new ItemSetIndexBuilder();
+          Map<String, SummaryStatistics> stats = builder.buildIndex(
+              freqPattsPath, new File(intervalOut, "index"),
+              Long.parseLong(intervalStartIn.getName()),
+              Long.parseLong(inervalEndIn.getName()), null);
+          
+          for (String key : stats.keySet()) {
+            FileUtils.writeStringToFile(new File(new File(
+                intervalOut, "stats"), key), stats.get(key)
+                .toString());
+          }
+        }
+        
+      }
+    }
+    
+  }
+  
 }
