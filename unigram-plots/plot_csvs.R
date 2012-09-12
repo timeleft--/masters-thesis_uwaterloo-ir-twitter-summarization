00414db2ad2b6782142fe0d6c2f55934251df260
yaboulna <yaboulna@scspc400.cs.uwaterloo.ca>
2012-09-12 16:19:02 -0400

    go home

--- unigram-plots/plot_csvs.R
+++ unigram-plots/plot_csvs.R
@@ -4,16 +4,15 @@
 ###############################################################################
 
 require(reshape)
+require(MARSS)
 setwd("/u2/yaboulnaga/data/twitter-trec2011/timeseries")
 hrs.files <- list.files()
-#hrs.list <- NULL
-#for(i in 1:length(hrs.files)) hrs.list[[i]] <- read.table(hrs.files[i], header=TRUE, sep='\t', quote="\"")
-# counts <- rbind.fill(hrs.list)
 counts <- NULL
 for(i in 1:length(hrs.files)) counts <- rbind.fill(counts, read.table(hrs.files[i], header=TRUE, sep='\t', quote="\"")[c("TIMESTAMP", "the", "of")])
 
-plot(counts$TIMESTAMP,log2(counts$the),type="l",pch="o",col="red")
+plot(counts$TIMESTAMP,counts$the,type="l",pch="o",col="red",ylim=c(0,80)) #,xaxp=c(counts$TIMESTAMP[2],tail(counts$TIMESTAMP,n=2)[1],16),xlab=)
+grid(17,8)
 #abline(lm(log2(the) ~ TIMESTAMP, data=counts))
 
-lines(counts$TIMESTAMP,log2(counts$of),type="l", pch="x",col="blue")
+#lines(counts$TIMESTAMP,log2(counts$of),type="l", pch="x",col="blue")
 
