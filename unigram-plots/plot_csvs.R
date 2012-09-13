5aa8f8c82d6303d271796e3389b08c41432486df
Younos Aboulnaga <yia@Younoss-MacBook-Air.local>
2012-09-12 22:42:21 -0400

    My first functions.. sums n Rows and returns the aggregated dataframe

--- unigram-plots/plot_csvs.R
+++ unigram-plots/plot_csvs.R
@@ -6,6 +6,17 @@
 require(reshape)
 require(MARSS)
 setwd("/u2/yaboulnaga/data/twitter-trec2011/timeseries")
+
+sumN <- function (inFrame, colname, n) {
+	len <- dim(inFrame)[1]
+	if(len %% n != 0)
+		stop("n must be a divisor for inFrame's length")
+	
+	retVal <- data.frame(TIMESTAMP=inFrame$TIMESTAMP[seq(n,len,by=n)])
+	retVal[colname] <- colSums(matrix(inFrame[[colname]], nrow=n))
+	return(retVal)
+}
+
 hrs.files <- list.files()
 counts <- NULL
 for(i in 1:length(hrs.files)) counts <- rbind.fill(counts, read.table(hrs.files[i], header=TRUE, sep='\t', quote="\"")[c("TIMESTAMP", "the", "of")])
