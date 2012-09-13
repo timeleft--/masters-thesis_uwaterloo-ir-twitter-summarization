5ed37b61e0f28422deb197d0a83e53824f1fbfe2
yaboulna <yaboulna@scspc400.cs.uwaterloo.ca>
2012-09-13 09:20:28 -0400

    Appends NAs if the dataframe length is not divisible by n

--- unigram-plots/plot_csvs.R
+++ unigram-plots/plot_csvs.R
@@ -5,24 +5,41 @@
 
 require(reshape)
 require(MARSS)
+require(ggplot2)
+
 setwd("/u2/yaboulnaga/data/twitter-trec2011/timeseries")
+kTS <- "TIMESTAMP"
+kUnigram <- "the"
+kEpochMins <- 5
 
 sumN <- function (inFrame, colname, n) {
 	len <- dim(inFrame)[1]
-	if(len %% n != 0)
-		stop("n must be a divisor for inFrame's length")
+	if(len %% n != 0) {
+    xlen <- ceiling(len/n)*n
+    deltaT <- inFrame$TIMESTAMP[2] - inFrame$TIMESTAMP[1]
+    currT <- inFrame$TIMESTAMP[len]
+    for(i in (len+1):xlen)  {
+      currT <- currT + deltaT
+      inFrame[i,] <- c(currT,NA)
+    }
+    len <-xlen
+  }
 	
 	retVal <- data.frame(TIMESTAMP=as.POSIXct(inFrame$TIMESTAMP[seq(n,len,by=n)],origin="1970-01-01", tz="GMT"))
-	retVal[colname] <- colSums(matrix(inFrame[[colname]], nrow=n))
+	retVal[colname] <- colSums(matrix(inFrame[[colname]], nrow=n),na.rm=TRUE)
 	return(retVal)
 }
+#debug(sumN)
+# setBreakpoint("plot_csvs.R#19")
 
-hrs.files <- list.files()
+hrs.files <- list.files(pattern=".*csv$")
 counts <- NULL
-for(i in 1:length(hrs.files)) counts <- rbind.fill(counts, read.table(hrs.files[i], header=TRUE, sep='\t', quote="\"")[c("TIMESTAMP", "the", "of")])
+for(i in 1:length(hrs.files)) counts <- rbind.fill(counts, sumN(read.table(hrs.files[i], header=TRUE, sep='\t', quote="\"")[c(kTS, kUnigram)],kUnigram,kEpochMins))
 
-plot(counts$TIMESTAMP,counts$the,type="l",pch="o",col="red",ylim=c(0,80)) #,xaxp=c(counts$TIMESTAMP[2],tail(counts$TIMESTAMP,n=2)[1],16),xlab=)
-grid(17,8)
+# qplot(~counts[[kUnigram]]=counts[[kTS]])
+
+#plot(counts$TIMESTAMP,counts$the,type="l",pch="o",col="red",ylim=c(0,80)) #,xaxp=c(counts$TIMESTAMP[2],tail(counts$TIMESTAMP,n=2)[1],16),xlab=)
+#grid(17,8)
 #abline(lm(log2(the) ~ TIMESTAMP, data=counts))
 
 #lines(counts$TIMESTAMP,log2(counts$of),type="l", pch="x",col="blue")
