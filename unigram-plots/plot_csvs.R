ca56b242368ecb40a33f9cd96540c3c9a3287fd7
yaboulna <yaboulna@scspc400.cs.uwaterloo.ca>
2012-09-13 10:38:14 -0400

    Trying to modify the plot but in vain

--- unigram-plots/plot_csvs.R
+++ unigram-plots/plot_csvs.R
@@ -9,7 +9,7 @@ require(ggplot2)
 
 setwd("/u2/yaboulnaga/data/twitter-trec2011/timeseries")
 kTS <- "TIMESTAMP"
-kUnigram <- "oprah"
+kUnigram <- "and"
 kEpochMins <- 5
 
 sumN <- function (inFrame, colname, n) {
@@ -25,7 +25,8 @@ sumN <- function (inFrame, colname, n) {
     len <-xlen
   }
 	
-	retVal <- data.frame(TIMESTAMP=as.POSIXct(inFrame$TIMESTAMP[seq(n,len,by=n)],origin="1970-01-01", tz="GMT"))
+	retVal <- data.frame(TIMESTAMP=as.POSIXct(inFrame$TIMESTAMP[seq(n,len,by=n)],origin="1970-01-01",
+          tz="GMT"))
 	retVal[colname] <- colSums(matrix(inFrame[[colname]], nrow=n),na.rm=TRUE)
 	return(retVal)
 }
@@ -34,8 +35,13 @@ sumN <- function (inFrame, colname, n) {
 
 hrs.files <- list.files(pattern=".*csv$")
 counts <- NULL
-for(i in 1:length(hrs.files)) counts <- rbind.fill(counts, sumN(read.table(hrs.files[i], header=TRUE, sep='\t', quote="\"")[c(kTS, kUnigram)],kUnigram,kEpochMins))
+for(i in 1:length(hrs.files)){ 
+  counts <- rbind.fill(counts, 
+      sumN(read.table(hrs.files[i], header=TRUE, sep='\t', quote="\"")
+        [c(kTS, kUnigram)],kUnigram,kEpochMins))
+}
 
-qplot(get(kTS), get(kUnigram), data=counts, xlab="Date/Time", ylab=kUnigram, log="y")
+qplot(get(kTS), get(kUnigram), data=counts, xlab="Date/Time", ylab=kUnigram, log="y") 
+# Can't control point size or shape :( -->   size=get(kUnigram)) + scale_size(c(0.20,0.21)) cex=.1) 
 
 
