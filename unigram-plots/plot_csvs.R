cec0c5dae362108deee2432a1a93d49e10b410d0
yaboulna <yaboulna@scspc400.cs.uwaterloo.ca>
2012-09-22 01:26:36 -0400

    The model can fit past data.. now it's time to forecast

--- unigram-plots/plot_csvs.R
+++ unigram-plots/plot_csvs.R
@@ -4,12 +4,14 @@
 ###############################################################################
 
 require(reshape)
-require(MARSS)
 require(ggplot2)
+#require(MARSS)
+require(dlmodeler)
+
 
 setwd("/u2/yaboulnaga/data/twitter-trec2011/timeseries")
 kTS <- "TIMESTAMP"
-kUnigram <- "and"
+kUnigram <- "the"
 kEpochMins <- 5
 
 sumN <- function (inFrame, colname, n) {
@@ -34,14 +36,50 @@ sumN <- function (inFrame, colname, n) {
 # setBreakpoint("plot_csvs.R#19")
 
 hrs.files <- list.files(pattern=".*csv$")
-counts <- NULL
+uniCntT <- NULL
 for(i in 1:length(hrs.files)){ 
-  counts <- rbind.fill(counts, 
+  uniCntT <- rbind.fill(uniCntT, 
       sumN(read.table(hrs.files[i], header=TRUE, sep='\t', quote="\"")
         [c(kTS, kUnigram)],kUnigram,kEpochMins))
 }
+kTraining <- dim(uniCntT)[1] #/2
 
-qplot(get(kTS), get(kUnigram), data=counts, xlab="Date/Time", ylab=kUnigram, log="y") 
-# Can't control point size or shape :( -->   size=get(kUnigram)) + scale_size(c(0.20,0.21)) cex=.1) 
+kModelName <- "level+trend+days"
+uniModel <- dlmodeler.build.structural(
+                            pol.order=1,
+                            pol.sigmaQ=NA,
+                            tseas.order=3, # when increased the processing time increases a lot
+                            tseas.period=24*(60/kEpochMins),
+                            tseas.sigmaQ=0,
+                            # day of week seasonal causes the diffuse to fail because of negative
+                            # variance (cycle) or hangs up the computer (dummy seasonal)
+                            #dseas.period=7*24*(60/kEpochMins),
+                            #dseas.sigmaQ=0,
+                            sigmaH=NA,
+                            name=kModelName)
+system.time(uniFit <- dlmodeler.fit(t(as.matrix(uniCntT[1:kTraining,kUnigram])), uniModel, method="MLE"))
+
+uniFit$model$Ht
+uniFit$model$Qt
+
+system.time(uniFilter <- dlmodeler.filter(t(as.matrix(uniCntT[1:kTraining,kUnigram])),uniFit$model,smooth=FALSE))
+
+kComp <- kModelName
+#compnames can be "level+trend+hourly"(kModelName) or "level+trend" or "seasonal"
+uniComp <- dlmodeler.extract(uniFilter,uniFit$model,type="observation", compnames=kComp, value="interval")
+uniSeasonal <- dlmodeler.extract(uniFilter,uniFit$model,type="observation", compnames="seasonal", value="mean")
+uniCycle <- dlmodeler.extract(uniFilter,uniFit$model,type="observation", compnames="cycle", value="mean")
+
+#qplot(get(kTS), get(kUnigram), data=uniCntT, xlab="Date/Time", ylab=kUnigram, log="y")  
+# Can't control point size or shape :( -->   size=get(kUnigram)) + scale_size(c(0.20,0.21)) cex=.1)
+#TODO: + geom_line(uniCntT[(kTraining+1):dim(uniCntT)[1],kTS],uniComp$"level+trend+hourly"[(1):(dim(uniCntT)[1]-kTraining)])
+plot(as.matrix(uniCntT[kUnigram]),type="p",log="y") #,ylim=c(0,400))
+lines(uniComp[[kComp]]$lower[1,],col="blue",lty=2)
+lines(uniComp[[kComp]]$mean[1,],col="red")
+lines(uniComp[[kComp]]$upper[1,],col="blue",lty=2)
+
+plot(as.matrix(uniCntT[1:kTraining,kUnigram]) - uniComp[[kComp]]$mean[1,1:kTraining],type="l",ylim=c(-100,100))
+lines(uniCycle$cycle[1:kTraining],type='l',col="green")
 
 
+#lines((kTraining+1):dim(uniCntT)[1],uniComp$"level+trend+hourly"[(1):(dim(uniCntT)[1]-kTraining)],col="red")
