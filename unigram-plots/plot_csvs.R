52c74189aa9cce77215ad4d08ceeafab6163395f
yaboulna <yaboulna@scspc400.cs.uwaterloo.ca>
2012-09-30 19:46:20 -0400

    The best model I could get

--- unigram-plots/plot_csvs.R
+++ unigram-plots/plot_csvs.R
@@ -11,14 +11,20 @@ require(dlmodeler)
 
 setwd("/u2/yaboulnaga/data/twitter-trec2011/timeseries")
 kTS <- "TIMESTAMP"
-kUnigram <- "egyptian"
+kUnigram <- "sister"
 #kEpochMins <- 5
 kSupport <- 50 # must be greater than kNormalityAssumptionThreshold = 30
+#kSuppLagExpW <- 0.8
 kFitMethod <- "MLE"
+# FKF is the best, FKAS is also good
+# but dlm is so heavy weight that day of week seasonal causes the diffuse to fail because of negative
+# variance (cycle) or hangs up the computer (dummy seasonal)
 kBackEnd <- "FKF" # KFAS, FKF or dlm
 kRawResult <- TRUE
-kModelName <- "level+days"
+kModelName <- "random-walk+days" #-trend+weeks+days"
 kComp <- "level"
+kRepeating<-"cycle" # "seasonal+cycle"
+
 
 
 supportLag <- function(inFrame, colname, supp) {
@@ -70,13 +76,32 @@ for(i in 1:length(hrs.files)){
         read.table(hrs.files[i], header=TRUE, sep='\t', quote="\"")
         [c(kTS, kUnigram)])
 }
+
+# determing the epoch length
 suppLag <- supportLag(uniCntT, kUnigram, kSupport)
-kEpochMins <- ceiling(as.numeric(quantile(suppLag, probs=c(0.75))))
-#TODO: assert that the minutes are less than a day, or else the cycle will have not meaning
+##The 3rd quartile of the suppLag
+#kEpochMins <- quantile(suppLag, probs=c(0.75))
+
+# The maximum likelihood rate assuming an exponential distrib
+kEpochMins <- mean(suppLag)
+
+##Exponentially moving average of the support lag
+#kEpochMins <- suppLag[1]
+#for(i in 2:length(suppLag)){
+#  kEpochMins <- kEpochMins + kSuppLagExpW * (suppLag[i] - kEpochMins)
+#}
+
+##Linear Weighted moving average of the support lag
+#require(TTR)
+#lastN <- length(suppLag)
+#kEpochMins <- WMA(suppLag,lastN,1:lastN)[lastN]
+
+# assert that the minutes are less than a day, or else the cycle will have not meaning
+kEpochMins <- min(c(ceiling(as.numeric(kEpochMins)),24*60))
+
+#Aggregate the epochs
 uniCntT <- sumN(uniCntT, kUnigram, kEpochMins)
-
 kTraining <- dim(uniCntT)[1] #/2
-
 uniCntM <- t(as.matrix(uniCntT[1:kTraining,kUnigram]))
 
 #sdNoise <- 0 #deterministic: caused very noisy curve 
@@ -85,20 +110,24 @@ uniCntM <- t(as.matrix(uniCntT[1:kTraining,kUnigram]))
 
 rm(uniModel)
 uniModel <- dlmodeler.build.structural(
-                            pol.order=0, #1,
-                            pol.sigmaQ=NA, #c(NA,0), 
+                            pol.order=0, #1 -> trend isn't large enough to justify this extra state 
+                            pol.sigmaQ=NA, #c(NA,0), -> Even as deterministic trend, it doesn't work
+                            #daily repeating as harmonics in a spectral component
                             tseas.order=3, # when increased the processing time increases a lot
-                            tseas.period=24*(60/kEpochMins),
+                            tseas.period=as.numeric(ceiling(24*(60/kEpochMins))),
                             tseas.sigmaQ=0,
-                            # day of week seasonal causes the diffuse to fail because of negative
-                            # variance (cycle) or hangs up the computer (dummy seasonal)
-                            #dseas.period=7*24*(60/kEpochMins),
+                            #weekly repeating as harmonics in a spectral component
+                            #tseas.order=7, # when increased the processing time increases a lot
+                            #tseas.period=as.numeric(ceiling(7*24*(60/kEpochMins))),
+                            #tseas.sigmaQ=0,
+                            #daily repeating as dummy seasonal
+                            #dseas.order=max(2,as.numeric(ceiling(24*(60/kEpochMins)))),
                             #dseas.sigmaQ=0,
                             sigmaH=NA,
                             name=kModelName)
 rm(uniFit)
 
-sink(paste("~/Desktop/", kUnigram, "_", kModelName, ".log", sep=""))
+sink(paste("~/Desktop/",kModelName,"_",  kUnigram, ".log", sep=""))
 print(paste("Epoch in minutes:", kEpochMins))
 print(paste("Time for", kFitMethod, "of model parameters (initialization) using backend", kBackEnd))
 print(system.time(uniFit <- dlmodeler.fit(uniCntM, uniModel, 
@@ -138,7 +167,8 @@ print(system.time(uniFilter <- dlmodeler.filter(uniCntM,uniFit$model,smooth=FALS
 # find out using: summary(uniFit$model$components)
 uniComp <- dlmodeler.extract(uniFilter,uniFit$model,type="observation", compnames=kComp, value="interval")
 #uniSeasonal <- dlmodeler.extract(uniFilter,uniFit$model,type="observation", compnames="seasonal", value="mean")
-uniCycle <- dlmodeler.extract(uniFilter,uniFit$model,type="observation", compnames="cycle", value="mean")
+#uniCycle <- dlmodeler.extract(uniFilter,uniFit$model,type="observation", compnames="cycle", value="mean")
+uniRepeating <- dlmodeler.extract(uniFilter,uniFit$model,type="observation", compnames=kRepeating, value="mean")
 uniAll <- dlmodeler.extract(uniFilter,uniFit$model,type="observation", compnames=kModelName, value="mean")
 
 
@@ -227,7 +257,7 @@ print("******************** ********************************************** *****
 sink()
 
 #if(library(corrgram, logical.return=TRUE)){
-#  pdf(paste("~/Desktop/", kUnigram, "_", "correlogram-stderr-filter", ".pdf", sep=""))
+#  pdf(paste("~/Desktop/",kModelName,"_",  kUnigram, "_", "correlogram-stderr-filter", ".pdf", sep=""))
 #
 #  corrgram(t(stdzdErr), order=FALSE, lower.panel=panel.shade, upper.panel=panel.pie, text.panel=panel.txt, main="Autocorrelation of standardized onestep ahead prediction error")
 #    
@@ -238,7 +268,7 @@ sink()
 # Can't control point size or shape :( -->   size=get(kUnigram)) + scale_size(c(0.20,0.21)) cex=.1)
 #TODONOT: + geom_line(uniCntT[(kTraining+1):dim(uniCntT)[1],kTS],uniComp$"level+trend+hourly"[(1):(dim(uniCntT)[1]-kTraining)])
 
-pdf(paste("~/Desktop/", kUnigram, "_", kComp, ".pdf", sep=""))
+pdf(paste("~/Desktop/",kModelName,"_",  kUnigram, "_", kComp, ".pdf", sep=""))
 
 dayDelims = seq(from=0,to=dim(uniCntT)[1],by=24*(60/kEpochMins));
 
@@ -252,7 +282,7 @@ par(mar = mar.default + c(7,0,0,0))
 plot(t(uniCntM),type="p",
     cex=0.5,pch=20,
     ylab=paste("Occurences of '", kUnigram, "' per ", kEpochMins, " mins"), xlab="", #"Date/Time",
-    main=paste(kUnigram, " occurrences, fitted model (red) and Confidence bands (blue)"),
+    main=paste(kUnigram, " occurrences, ", kComp, " (red) and Confidence bands (blue)"),
     lab=c(1,10,7)) # ,ylim=uniYLims) #,log="y") #,ylim=c(0,400))
 lines(uniComp[[kComp]]$lower[1,],col="blue",lty=2)
 lines(uniComp[[kComp]]$mean[1,],col="red")
@@ -262,7 +292,7 @@ axis(1,at=dayDelims,tck=1,lty=3,labels=uniCntT[[kTS]][dayDelims+1],las=2)
 
 dev.off()
 
-pdf(paste("~/Desktop/", kUnigram, "_", "irregular+cycle", ".pdf", sep=""))
+pdf(paste("~/Desktop/", kModelName,"_", kUnigram, "_", "irregular+repeating", ".pdf", sep=""))
 
 # noiseYLim <- ???
 # noiseYLog <- flase (-ve numbers)
@@ -272,12 +302,12 @@ plot(oneStepAheadPredictionErr,type="l",
     ylab=paste("Occurences of '", kUnigram, "' per ", kEpochMins, " mins"), xlab="", #"Date/Time",
     main=paste(kUnigram, "Irregular (black) and daily Cycle (green)"),
     ylim=c(-kSupport,kSupport),lab=c(1,10,7))
-lines(uniCycle$cycle[1:kTraining],type='l',col="green")
+lines(uniRepeating[[kRepeating]][1:kTraining],type='l',col="green")
 axis(1,at=dayDelims,tck=1,lty=3,labels=uniCntT[[kTS]][dayDelims+1],las=2)
 
 dev.off()
 
-pdf(paste("~/Desktop/", kUnigram, "_", kComp, "_", kModelName, ".pdf", sep=""))
+pdf(paste("~/Desktop/", kModelName,"_", kUnigram, "_", "full-predicted", ".pdf", sep=""))
 par(mar = mar.default + c(7,0,0,0))
 plot(t(uniCntM), type="p", cex=0.5,pch=20,
   ylab=paste("Occurences of '", kUnigram, "' per ", kEpochMins, " mins"), xlab="",
