52dd79e478b48d2bb8e12dc1885dfa93ef13686d
Younos Aboulnaga <yia@cs-auth-dc-129-97-60-187.dynamic.uwaterloo.ca>
2012-11-22 15:29:24 -0500

    Tokenizing Tweets Pig way

---
+++ pig_scripts/inverted-index-builder.pig
@@ -0,0 +1,8 @@
+-- Builds an inverted index where each column is the posting list of a term (the name of the column)
+-- Rows are timestamps, and a field names TIMESTAMP indicate what time these Tweets were created
+-- In each cell we store an array of the last part of the TweetIds in which the column term occurred
+-- There are more columns for statistics, such as total term count, average tweet length, tweet count
+
+REGISTER yaboulna-udf.jar;
+tweets = LOAD 'file:///u2/yaboulnaga/data/twitter-tracked/debug/[^_]*/[^.]*[^g]' USING PigStorage('\t') AS (id:long, screenname:chararray, timestamp:long, tweet:chararray); --debug XOR spritzer_unsorted_csv
+tokens = FOREACH tweets GENERATE timestamp, id, yaboulna.pig.TweetTokenizer(tweet);
--- pig_udf/src/main/java/yaboulna/pig/TweetTokenizer.java
+++ pig_udf/src/main/java/yaboulna/pig/TweetTokenizer.java
@@ -4,18 +4,22 @@
 package yaboulna.pig;
 
 import java.io.IOException;
+import java.util.Set;
 
 import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.pig.EvalFunc;
 import org.apache.pig.data.Tuple;
+import org.apache.pig.data.TupleFactory;
 
 import ca.uwaterloo.twitter.TokenIterator.LatinTokenIterator;
+
+import com.google.common.collect.Sets;
 
 /**
  * @author yaboulna
  * 
  */
-public class TweetTokenizer extends EvalFunc<String> {
+public class TweetTokenizer extends EvalFunc<Tuple> {
   
   /**
    * 
@@ -30,14 +34,29 @@ public class TweetTokenizer extends EvalFunc<String> {
    */
 
   @Override
-  public String exec(Tuple input) throws IOException {
+  public Tuple exec(Tuple input) throws IOException {
     if (input == null || input.size() == 0)
       return null;
     try {
       String tweet = StringEscapeUtils.unescapeJava((String) input.get(0));
+      
       LatinTokenIterator tokenIter = new LatinTokenIterator(tweet);
       tokenIter.setRepeatHashTag(true);
       tokenIter.setRepeatedHashTagAtTheEnd(true);
+      
+      Set<String> resSet = Sets.newHashSet();
+      while(tokenIter.hasNext()){
+    	  resSet.add(tokenIter.next());
+      }
+      
+      Tuple result = TupleFactory.getInstance().newTuple(resSet.size());
+      int i=0;
+      for(String token: resSet){
+    	  result.set(i++, token);
+      }
+      
+      return result;
+      
     } catch (Exception e) {
       throw new IOException("Caught exception processing input row " + input.toDelimitedString("\t"), e);
     }
