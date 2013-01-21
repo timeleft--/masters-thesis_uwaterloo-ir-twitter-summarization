442e3d9bfc5f158e53c9f5cdf37193cf7796afe1
yaboulna <yaboulna@uwaterloo.ca>
2013-01-21 03:06:48 -0500

    Unit Testing the reducer, including the JDBC conncetion

--- postgres_insert-ngrams/src/main/java/ca/uwaterloo/yaboulna/InsertNGramsReducer.java
+++ postgres_insert-ngrams/src/main/java/ca/uwaterloo/yaboulna/InsertNGramsReducer.java
@@ -3,6 +3,7 @@ package ca.uwaterloo.yaboulna;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
@@ -27,56 +28,57 @@ public class InsertNGramsReducer extends
 
       String url = "jdbc:postgresql://localhost:5433/spritzer";
       Properties props = new Properties();
-      props.setProperty("user", "yaboulna");
-      props.setProperty("password", "UWPa55w0rt");
-      props.setProperty("ssl", "false");
+      props.setProperty("user", "uspritzer");
+      props.setProperty("password", "Spritz3rU");
+//      props.setProperty("ssl", "false");
       Connection conn = DriverManager.getConnection(url, props);
 
       conn.setAutoCommit(false);
 
       Statement stmt = conn.createStatement();
-      try{
-      String ngramTableName = "ngrams_" + keyIn.getLeftElement();
-      String htagTableName = "htags_" + keyIn.getLeftElement();
+      try {
+        String ngramTableName = "ngrams_" + keyIn.getLeftElement();
+        String htagTableName = "htags_" + keyIn.getLeftElement();
 
-      stmt.execute("CREATE UNLOGGED TABLE "
-          + ngramTableName
-          + " (id int8, timeMillis timestamp, date int4, ngram text[], ngramLen int2, tweetLen int2, position int2)");
+        stmt.execute("CREATE UNLOGGED TABLE "
+            + ngramTableName
+            + " (id int8, timeMillis timestamp, date int4, ngram text[], ngramLen int2, tweetLen int2, position int2)");
 // stmt.execute("CREATE INDEX " +ngramTableName+"_date ON " + ngramTableName +"(date)");
 
-      stmt.execute("CREATE UNLOGGED TABLE "
-          + htagTableName
-          + " (id int8, timeMillis timestamp, date int4, ngram text[], ngramLen int2, tweetLen int2, position int2)");
+        stmt.execute("CREATE UNLOGGED TABLE "
+            + htagTableName
+            + " (id int8, timeMillis timestamp, date int4, ngram text[], ngramLen int2, tweetLen int2, position int2)");
 // stmt.execute("CREATE INDEX " +htagTableName+"_date ON " + htagTableName +"(date)");
 
-      int count = 0;
-      for (Record value : valuesIn) {
-        String tablename;
-        if (value.position == value.tweetLen) {
-          // hashtag
-          tablename = htagTableName;
-        } else {
-          tablename = ngramTableName;
-        }
-        stmt.addBatch("INSERT INTO " + tablename + " VALUES("
-            + value.id + ","
-            + value.timeMillis + ","
-            + value.date + ","
-            + toSQLArray(value.ngram) + ","
-            + value.ngramLen + ","
-            + value.tweetLen + ","
-            + value.position
-            + ")");
-        if (++count > 1000) {
-          count = 0;
-          stmt.executeBatch();
-          stmt.clearBatch();
+        int count = 0;
+        for (Record value : valuesIn) {
+          String tablename;
+          if (value.position == value.tweetLen) {
+            // hashtag
+            tablename = htagTableName;
+          } else {
+            tablename = ngramTableName;
+          }
+          stmt.addBatch("INSERT INTO " + tablename + " VALUES("
+              + value.id + ","
+              + value.timeMillis + ","
+              + value.date + ","
+              + toSQLArray(value.ngram) + ","
+              + value.ngramLen + ","
+              + value.tweetLen + ","
+              + value.position
+              + ")");
+          if (++count > 1000) {
+            count = 0;
+            stmt.executeBatch();
+            stmt.clearBatch();
 // stmt.clearParameters();
+          }
         }
-      }
-      stmt.executeBatch();
-      }finally{
+        stmt.executeBatch();
+      } finally {
         stmt.close();
+        conn.close();
       }
     } catch (SQLException e) {
       throw new IOException(e);
@@ -97,12 +99,50 @@ public class InsertNGramsReducer extends
     return sqlArrayBuilder.toString();
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    InsertNGramsReducer obj = new InsertNGramsReducer();
 
-    String[] ngrams = {"you", "your mother", "your dog"};
-    System.out.println(new InsertNGramsReducer().toSQLArray(ngrams));
+    Class.forName("org.postgresql.Driver");
 
-    String[] ngrams2 = {"me", "my father", "my cat"};
-    System.out.println(new InsertNGramsReducer().toSQLArray(ngrams2));
+    String url = "jdbc:postgresql://hops.cs.uwaterloo.ca:5433/spritzer";
+    Properties props = new Properties();
+    props.setProperty("user", "uspritzer");
+    props.setProperty("password", "Spritz3rU");
+//    props.setProperty("ssl", "false");
+    Connection conn = DriverManager.getConnection(url, props);
+
+    conn.setAutoCommit(false);
+
+    Statement stmt = conn.createStatement();
+    try {
+      stmt.execute("CREATE TABLE test(arr text[])");
+
+      String[] ngrams = {"you", "your mother", "your dog"};
+      stmt.addBatch("INSERT INTO test VALUES(" + obj.toSQLArray(ngrams) + ")");
+
+      ResultSet ret = stmt.executeQuery("Select * from test");
+      while (ret.next()) {
+        System.out.println(ret.getArray(1));
+      }
+
+      String[] ngrams2 = {"me", "my father", "my cat"};
+      stmt.addBatch("INSERT INTO test VALUES(" + obj.toSQLArray(ngrams2) + ")");
+
+      ret = stmt.executeQuery("Select * from test");
+      while (ret.next()) {
+        System.out.println(ret.getArray(1));
+      }
+
+      stmt.executeBatch();
+
+      ret = stmt.executeQuery("Select * from test");
+      while (ret.next()) {
+        System.out.println(ret.getArray(1));
+      }
+      stmt.clearBatch();
+    } finally {
+      stmt.close();
+      conn.close();
+    }
   }
 }
