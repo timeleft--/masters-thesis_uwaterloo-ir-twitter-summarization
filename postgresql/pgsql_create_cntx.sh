9e2946f200475038c898b0b04c46070bbd116d93
yaboulna <yaboulna@scspc400.cs.uwaterloo.ca>
2013-01-31 11:08:12 -0500

    Reshaped the data frame to create congingency table.. I'm still not sure about the column names, and whether I should make them a list but at least I could get it running

--- postgresql/pgsql_create_cntx.sh
+++ postgresql/pgsql_create_cntx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $# -ne 2 ]; then
+if [ $# -ne 4 ]; then
     echo "usage"
     exit 1
 fi
@@ -10,17 +10,21 @@ for epoch in '5min' '1hr' '1day' # More than a day date becomes meaningless '1we
 do
     #echo "${psql} 'DROP TABLE IF EXISTS cnt_${epoch} CASCADE;'"
     echo "${psql} 'CREATE UNLOGGED TABLE cnt_${epoch} (ngramLen int2, ngramArr text[], date int4, epochStartMillis int8, cnt int4);'" #, pkey serial Primary key);'"
-    for len in 1 2  #{1..71} 
+#    for len in 1 2  #{1..71}
+    len=$3
+    while [ $len -le $4 ] 
     do
 	# echo "hdfs dfs -cat ${root}/cnt_${epoch}/ngrams1/part* | ${psql} 'COPY cnt_${epoch} (ngram, date, epochstartmillis, cnt) FROM STDIN;'"
-	echo "${psql} 'CREATE UNLOGGED TABLE cnt_${epoch}${len}_staging (ngramLen int2, ngram text, date int4, epochStartMillis int8, cnt int4);'"
-	echo "${psql} \"COPY cnt_${epoch}${len}_staging (ngramLen, ngram, date, epochstartmillis, cnt) FROM '${root}cnt_${epoch}_onefile/ngrams${len}';\""
+#	echo "${psql} 'CREATE UNLOGGED TABLE cnt_${epoch}${len}_staging (ngramLen int2, ngram text, date int4, epochStartMillis int8, cnt int4);'"
+#	echo "${psql} \"COPY cnt_${epoch}${len}_staging (ngramLen, ngram, date, epochstartmillis, cnt) FROM '${root}cnt_${epoch}_onefile/ngrams${len}';\""
 	echo "${psql} \"CREATE UNLOGGED TABLE cnt_${epoch}${len} as select ngramlen, regexp_split_to_array(trim(trailing ')' from trim(leading '(' from ngram)), ',') as ngramArr, date, epochstartmillis, cnt from cnt_${epoch}${len}_staging;\"" # where date = 121221 and cnt > 3;'
 	echo "${psql} 'ALTER TABLE cnt_${epoch}${len} INHERIT cnt_${epoch};'"
 	echo "${psql} 'ALTER TABLE cnt_${epoch}${len} ALTER COLUMN ngramlen SET DEFAULT ${len};'"
-	echo "${psql} 'DROP TABLE cnt_${epoch}${len}_staging;'"
+#	echo "${psql} 'DROP TABLE cnt_${epoch}${len}_staging;'"
 	echo "${psql} 'CREATE INDEX cnt_${epoch}${len}_date ON cnt_${epoch}${len}(date);'"
 	echo "${psql} 'CREATE INDEX cnt_${epoch}${len}_ngramLen ON cnt_${epoch}${len}(ngramLen);'"
 	echo "${psql} 'CREATE UNLOGGED TABLE volume_${epoch}${len} as select  min(ngramLen) as ngramLen, epochstartmillis, sum(cnt) as totalcnt from cnt_${epoch}${len} group by epochstartmillis;'"
+	echo ""
+	len=`expr $len + 1`
     done
 done
--- twitterr/conttable_construct.R
+++ twitterr/conttable_construct.R
@@ -9,42 +9,72 @@ drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, dbname="sample-0.01", user="yaboulna", password="5#afraPG",
     host="hops.cs.uwaterloo.ca", port="5433")
 rs <- dbSendQuery(con,
-  " select a.epochstartmillis/1000 as epochstartux, 121221 as date, a.ngramarr as unigram, b.ngramarr as bigram, 
-       b.cnt as togethercnt, a.cnt - b.cnt as alonecnt, a.cnt as unigramcnt, v.totalcnt as epochvol, b.ngramlen as ngramlen 
+    "select a.epochstartmillis/1000 as epochstartux, 121221 as date, v.totalcnt as epochvol, b.ngramlen as ngramlen, 
+				b.ngramarr as ngram, b.cnt as togethercnt,
+				a.ngramarr as unigram, a.cnt - b.cnt as alonecnt, a.cnt as unigramcnt 
     from (cnt_5min1 a join cnt_5min2 b on 
-          a.epochstartmillis = b.epochstartmillis and a.ngramArr[1] = ANY (b.ngramarr))
+          a.epochstartmillis = b.epochstartmillis and a.ngramArr[1] = ANY (b.ngramarr)
+          and NOT a.ngramArr[1] = ALL (b.ngramarr))
        join volume_5min1 v on v.epochstartmillis = b.epochstartmillis
     where a.date = 121221 and b.date=121221 and b.cnt > 3;")
 
-#" select bigram, 121221 as date, array_agg(a.epochstartmillis) as epochstartuxArr, array_agg(unigram) AS unigramArr,
+#" select ngram, 121221 as date, array_agg(a.epochstartmillis) as epochstartuxArr, array_agg(unigram) AS unigramArr,
 #    array_agg(togetherCnt) as togetherCntArr, array_agg(aloneCnt) as aloneCntArr, 
 #    array_agg(unigramCnt) as unigramCntArr, array_agg(epochVol) as epochVolArr, array_agg(ngramLen) as ngramLenArr 		
-#    FROM (select a.epochstartmillis, a.ngramarr as unigram, b.ngramarr as bigram, 
+#    FROM (select a.epochstartmillis, a.ngramarr as unigram, b.ngramarr as ngram, 
 #    b.cnt as togethercnt, a.cnt - b.cnt as alonecnt, a.cnt as unigramcnt, v.totalcnt as epochvol, b.ngramlen as ngramlen 
 #    from ((cnt_5min1 a join cnt_5min2 b on 
 #    a.epochstartmillis = b.epochstartmillis and a.ngramArr[1] = ANY (b.ngramarr))
 #    join volume_5min1 v on v.epochstartmillis = b.epochstartmillis)
 #    where a.date = 121221 and b.date=121221 and b.cnt > 3 GROUP BY b.ngramarr) AS r;"
-
+require(plyr)
 df <- fetch(rs, n=-1);
 
-bigramOccs <- duplicated(df$epochstartux, df$bigram)
-comp1 <- df[!bigramOccs,]
-comp2 <- df[bigramOccs]
+numNGrams <- nrow(df) / df[1,"ngramlen"]
+if(numNGrams != floor(numNGrams)){
+  stop("There was a duplicate unigram in some ngrams and thus the code below will not work!
+In case of bigrams it was enough to append 'and NOT a.ngramArr[1] = ALL (b.ngramarr)' to the SQL")
+}
+#idata.frame(
+ngramGrps <- ddply(df, c("epochstartux","ngram"), function(bg){
+      bgRow <- bg[1,1:6]
+      for(i in 1:nrow(bg)) {
+#        if(!identical(bgRow[,1:6], bg[i,1:6])){
+        if(any(bgRow[,1:6] != bg[i,1:6])){
+          stop(paste("This is a debug message because the SQL and grouping produced ngram groups that differ at i =",
+                  i, str(bgRow[,1:6]), str(bg[i,1:6])))
+        }
+        bgRow[paste("unigram", i, sep=".")] = bg[i,"unigram"]
+        bgRow[paste("alonecnt", i, sep=".")] = bg[i,"alonecnt"]
+        bgRow[paste("unigramcnt", i, sep=".")] = bg[i,"unigramcnt"]
+      }
+      
+      return(bgRow)
+#      str(list(bg))
+#      str(join_all(as.list(bg), by=c("epochstartux","ngram")))
+    })
+
+#bigramOccs <- duplicated(df$epochstartux, df$bigram)
+#comp1 <- df[!bigramOccs,]
+#comp2 <- df[bigramOccs,]
+
 
 #bigrams = factor;
 #tapply(create congengency table with time)
 
 #trans <- transform(df, group = cbind(bigram,epochstartux))
 #df$bound <- with(df, cbind(togethercnt, alonecnt, unigramcnt, epochvol))
-#reshape from stat was the most intereting reshap(
+#reshape from stat was the most intereting:
+#reshape(bg,v.name="alonecnt",timevar=)
 #require(reshape)
 #bi <- cast(df, bigram~unigram+epochstartux+date,bound)
 
+#cleanup
+rm(df)
+# dbClearResult(rs, ...) flushes any pending data and frees the resources used by resultset. Eg.
+dbClearResult(rs)
 # dbDisconnect(con, ...) closes the connection. Eg.
 dbDisconnect(con)
-
 # dbUnloadDriver(drv,...) frees all the resources used by the driver. Eg.
-
 dbUnloadDriver(drv)
 
