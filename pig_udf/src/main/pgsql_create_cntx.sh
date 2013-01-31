d123fb38324c0b811d20fbeffbbf91a5ae721673
yaboulna <yaboulna@scspc400.cs.uwaterloo.ca>
2013-01-30 20:37:34 -0500

    Partitioning

--- pig_udf/src/main/pgsql_create_cntx.sh
+++ pig_udf/src/main/pgsql_create_cntx.sh
@@ -8,10 +8,15 @@ root=${2}
 psql="psql -p 5433 -d ${db} -c "
 for epoch in '5min' '1hr' '1day' # More than a day date becomes meaningless '1week' '1month'
 do
-    echo "${psql} 'DROP TABLE IF EXISTS cnt_${epoch};'"
-    echo "${psql} 'CREATE UNLOGGED TABLE cnt_${epoch} (ngramLen int2 DEFAULT 1, ngram text, date int4, epochStartMillis int8, cnt int4, pkey serial Primary key);'"
-# echo "hdfs dfs -cat ${root}/cnt_${epoch}/ngrams1/part* | ${psql} 'COPY cnt_${epoch} (ngram, date, epochstartmillis, cnt) FROM STDIN;'"
-    echo "${psql} \"COPY cnt_${epoch} (ngram, date, epochstartmillis, cnt) FROM '/home/yaboulna/vmshared/backup/cnt_${epoch}_onefile';\""
-    echo "${psql} 'CREATE INDEX cnt_${epoch}_date ON cnt_${epoch}(date);'"
-    echo "${psql} 'CREATE INDEX cnt_${epoch}_ngramLen ON cnt_${epoch}(ngramLen);'"
+    #echo "${psql} 'DROP TABLE IF EXISTS cnt_${epoch} CASCADE;'"
+    echo "${psql} 'CREATE UNLOGGED TABLE cnt_${epoch} (ngramLen int2, ngram text, date int4, epochStartMillis int8, cnt int4);'" #, pkey serial Primary key);'"
+    for len in 2  #{1..71} 
+    do
+	# echo "hdfs dfs -cat ${root}/cnt_${epoch}/ngrams1/part* | ${psql} 'COPY cnt_${epoch} (ngram, date, epochstartmillis, cnt) FROM STDIN;'"
+	echo "${psql} 'CREATE UNLOGGED TABLE cnt_${epoch}${len} (CHECK (ngramLen = ${len})) INHERITS(cnt_${epoch});'" 
+	echo "${psql} 'ALTER TABLE cnt_${epoch}${len} ALTER COLUMN ngramlen SET DEFAULT ${len};'"
+	echo "${psql} \"COPY cnt_${epoch}${len} (ngram, date, epochstartmillis, cnt) FROM '${root}cnt_${epoch}_onefile/ngrams${len}';\""
+	echo "${psql} 'CREATE INDEX cnt_${epoch}${len}_date ON cnt_${epoch}${len}(date);'"
+	echo "${psql} 'CREATE INDEX cnt_${epoch}${len}_ngramLen ON cnt_${epoch}${len}(ngramLen);'"
+    done
 done
