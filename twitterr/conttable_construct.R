b407159156e549fa7a65241b237de0dafc04f4b4
yaboulna <yaboulna@scspc400.cs.uwaterloo.ca>
2013-01-31 11:54:23 -0500

    construct contingency table as a function.. to source, don't forget to set working dir

--- twitterr/conttable_construct.R
+++ twitterr/conttable_construct.R
@@ -4,19 +4,24 @@
 # Author: yaboulna
 ###############################################################################
 
-require(RPostgreSQL)
+MILLIS_PUT_1000 <- 1;
+conttable_construct <- function(date, epoch1, ngramlen2, epoch2=NULL, ngramlen1=1, support=3) {
+if(is.null(epoch2)){
+  epoch2=epoch1
+}
+require(RPostgreSQL)  
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, dbname="sample-0.01", user="yaboulna", password="5#afraPG",
     host="hops.cs.uwaterloo.ca", port="5433")
 rs <- dbSendQuery(con,
-    "select a.epochstartmillis/1000 as epochstartux, 121221 as date, v.totalcnt as epochvol, b.ngramlen as ngramlen, 
+    sprintf("select a.epochstartmillis/1000 as epochstartux, %d as date, v.totalcnt as epochvol, b.ngramlen as ngramlen, 
 				b.ngramarr as ngram, b.cnt as togethercnt,
 				a.ngramarr as unigram, a.cnt - b.cnt as alonecnt, a.cnt as unigramcnt 
-    from (cnt_5min1 a join cnt_5min2 b on 
+    from (cnt_%s%d a join cnt_%s%d b on 
           a.epochstartmillis = b.epochstartmillis and a.ngramArr[1] = ANY (b.ngramarr)
           and NOT a.ngramArr[1] = ALL (b.ngramarr))
        join volume_5min1 v on v.epochstartmillis = b.epochstartmillis
-    where a.date = 121221 and b.date=121221 and b.cnt > 3;")
+    where a.date = %d and b.date=%d and b.cnt > %d;", date, epoch1, ngramlen1, epoch2, ngramlen2, date, date, support))
 
 require(plyr)
 df <- fetch(rs, n=-1);
@@ -26,32 +31,28 @@ if(numNGrams != floor(numNGrams)){
   stop("There was a duplicate unigram in some ngrams and thus the code below will not work!
 In case of bigrams it was enough to append 'and NOT a.ngramArr[1] = ALL (b.ngramarr)' to the SQL")
 }
-
-ngramGrps <- ddply(idata.frame(df), c("epochstartux","ngram"), function(bg){
+#idata.frame( object environment is not subsettable
+ngramGrps <- ddply(df, c("epochstartux","ngram"), function(bg){
       bgRow <- bg[1,1:6]
       for(i in 1:nrow(bg)) {
-##        if(!identical(bgRow[,1:6], bg[i,1:6])){
-##        if(any(as.list(bgRow[1,1:6]) != as.list(bg[i,1:6]))){
-##        if(any(bgRow[1,1:6] != bg[i,1:6]))
-#        #It alwas fails the first time complaining that comparison of these types is not implemented
-#        # or that comparison si for atimoc and list types
-#          stop(paste("This is a debug message because the SQL and grouping produced ngram groups that differ at i =",
-#                  i, str(bgRow[,1:6]), str(bg[i,1:6])))
-#        }
         bgRow[paste("unigram", i, sep=".")] = bg[i,"unigram"]
         bgRow[paste("alonecnt", i, sep=".")] = bg[i,"alonecnt"]
         bgRow[paste("unigramcnt", i, sep=".")] = bg[i,"unigramcnt"]
       }
-      
+      bgRow["POSIXtime"] <- as.POSIXct(bgRow[1,"epochstartux"]/MILLIS_PUT_1000,origin="1970-01-01",
+          tz="GMT")
       return(bgRow)
     }) #,.parallel = TRUE)  will use doMC to parallelize on a higher level then no need here 
 
 #cleanup
 rm(df)
 # dbClearResult(rs, ...) flushes any pending data and frees the resources used by resultset. Eg.
-dbClearResult(rs)
+try(dbClearResult(rs))
 # dbDisconnect(con, ...) closes the connection. Eg.
-dbDisconnect(con)
+try(dbDisconnect(con))
 # dbUnloadDriver(drv,...) frees all the resources used by the driver. Eg.
-dbUnloadDriver(drv)
+try(dbUnloadDriver(drv))
+
+return(ngramGrps)
+}
 
