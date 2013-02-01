11cda9c985b713fbd92b73fa2b2e88e69372aab0
yaboulna <yaboulna@scspc400.cs.uwaterloo.ca>
2013-02-01 11:03:35 -0500

    Modified for better usage

--- twitterr/conttable_construct.R
+++ twitterr/conttable_construct.R
@@ -22,7 +22,9 @@ support<-3
 epoch2<-NULL
 db<-"sample-0.01"
 retEpochGrps<-TRUE
-retNgramGrps<-FALSE
+retNgramGrps<-TRUE
+alignEpochs<-FALSE
+appendPosixTime<-FALSE
 }
 
 # Makes sure all epochs are the same length
@@ -46,12 +48,22 @@ align_epochs <- function(dframe, epoch) {
 }
 #debug(align_epochs)
 
+
+#####################################3
 toPosixTime <- function(timestamp){
   as.POSIXct(timestamp/MILLIS_PUT_1000,origin="1970-01-01", tz="GMT")
 }
 
+
+########################################################
+
+stripEndChars <- function(ngram) {
+  return(substring(ngram, 2, nchar(ngram)-1))
+}
+
+############################################
 conttable_construct <- function(date, epoch1, ngramlen2, epoch2=NULL, ngramlen1=1, support=3,
-  db="sample-0.01", retEpochGrps=FALSE, retNgramGrps=TRUE) {
+  db="sample-0.01", retEpochGrps=FALSE, retNgramGrps=TRUE, alignEpochs=FALSE, appendPosixTime=FALSE) {
   
   if(is.null(epoch2)){
     epoch2<-epoch1
@@ -70,37 +82,47 @@ conttable_construct <- function(date, epoch1, ngramlen2, epoch2=NULL, ngramlen1=
          join volume_5min1 v on v.epochstartmillis = b.epochstartmillis
       where a.date = %d and b.date=%d and b.cnt > %d;", date, epoch1, ngramlen1, epoch2, ngramlen2, date, date, support))
   
-  require(plyr)
   df <- fetch(rs, n=-1)
-  
+
   numNGrams <- nrow(df) / df[1,"ngramlen"]
   if(numNGrams != floor(numNGrams)){
     stop("There was a duplicate unigram in some ngrams and thus the code below will not work!
   In case of bigrams it was enough to append 'and NOT a.ngramArr[1] = ALL (b.ngramarr)' to the SQL")
   }
+  
+  df <- within(df,{unigram=stripEndChars(unigram)
+        ngram=stripEndChars(ngram)})
+  
+  require(plyr)
+  
   #idata.frame( object environment is not subsettable
   if(retNgramGrps){
     ngramGrps <- ddply(df, c("epochstartux","ngram"), function(bg){
         bgRow <- bg[1,1:6]
+         
         for(i in 1:nrow(bg)) {
           bgRow[paste("unigram", i, sep=".")] <- bg[i,"unigram"]
           bgRow[paste("alonecnt", i, sep=".")] <- bg[i,"alonecnt"]
           bgRow[paste("unigramcnt", i, sep=".")] <- bg[i,"unigramcnt"]
         }
-        bgRow["utctime"] <- toPosixTime(bgRow[1,"epochstartux"])
+        if(appendPosixTime)
+          bgRow["utctime"] <- toPosixTime(bgRow[1,"epochstartux"])
         return(bgRow)
       }) #,.parallel = TRUE)  will use doMC to parallelize on a higher level then no need here 
   
-    ngramGrps <- align_epochs(ngramGrps, epoch1)
+    if(alignEpochs)
+      ngramGrps <- align_epochs(ngramGrps, epoch1)
     
   }
+  
   if(retEpochGrps){
     createCooccurNooccur <- function(eg) {
     
-      egRow <- eg[1,1:3]
-      egRow["utctime"] <- toPosixTime(egRow[1,"epochstartux"])
+      egRow <- eg[1,1:4]
+      if(appendPosixTime)
+        egRow["utctime"] <- toPosixTime(egRow[1,"epochstartux"])
       
-      uniqueUgrams <- unique(eg[,"unigram"])
+      uniqueUgrams <- unique(eg[, "unigram"])
       
       nUnique <- length(uniqueUgrams)
       dnames <- c(uniqueUgrams, TOTAL)
@@ -110,7 +132,7 @@ conttable_construct <- function(date, epoch1, ngramlen2, epoch2=NULL, ngramlen1=
       #dimnames starts behaving wierdly after 5 iterations by using the string c(...) as the dimns!
       
       ixTOTAL <- ixLookup[TOTAL, 'ix']
-      cooccurs[ixTOTAL,ixTOTAL] <- eg[1,"epochvol"]
+#      cooccurs[ixTOTAL,ixTOTAL] <- eg[1,"epochvol"]
         
       countCooccurNooccurUnigram <- function(ug) {             
         ugram <- ug[1,"unigram"]
@@ -160,15 +182,10 @@ conttable_construct <- function(date, epoch1, ngramlen2, epoch2=NULL, ngramlen1=
           
           othersInNgram <- unlist(strsplit(ug[r,"ngram"],","))
           
-          othersInNgram[1] <- substring(othersInNgram[1],2)
-          othersInNgram[length(othersInNgram)] <- substring(othersInNgram[length(othersInNgram)],1,
-              nchar(othersInNgram[length(othersInNgram)])-1)
-          
-          othersInNgram <- setdiff(othersInNgram, substring(ugram, 2, nchar(ugram)-1))
-          
+          othersInNgram <- setdiff(othersInNgram, ugram)
           
           for(o in 1:length(othersInNgram)){
-            ugram2 <- paste("{",othersInNgram[o],"}", sep="")
+            ugram2 <-othersInNgram[o]
             
             ixugram2 <- ixLookup[ugram2, 'ix']
             
@@ -176,13 +193,15 @@ conttable_construct <- function(date, epoch1, ngramlen2, epoch2=NULL, ngramlen1=
             cooccurs[ixugram,ixugram2] <- cooccurs[ixugram,ixugram2] + cnt
             
             # decrease the occurrences of this bigram but not the other
-            notoccurs[ixugram,ixugram2] <-  notoccurs[ixugram,ixugram2] - cnt
+            # The division accounts for the repeated deduction of the cnt with each element of ngram
+            # The -1 accouts for the iteration that will be skipped which is that of ugram itself
+            notoccurs[ixugram,ixugram2] <-  notoccurs[ixugram,ixugram2] - (cnt/(ngramlen2-1))
 #            if(DEBUG){
-              if(notoccurs[ixugram,ixugram] < 0){
+              if(notoccurs[ixugram,ixugram2] < 0){
                 #lapply(c(cnt, ixugram,notoccurs[ixugram,ixugram],ugram, ugram2,ug[1,"epochstartux"]), str)
-                print(paste("WARNING: notoccurs negative after reducing cnt=",cnt, ixugram,notoccurs[ixugram,ixugram],ugram, ugram2,ug[1,"epochstartux"]))
+                print(paste("WARNING: notoccurs negative after reducing cnt=",cnt, ixugram,notoccurs[ixugram,ixugram2],ugram, ugram2,ug[1,"epochstartux"], str(othersInNgram)))
                 print(paste("------------------------------------------------------------------"))
-                notoccurs[ixugram,ixugram] <- 0
+                notoccurs[ixugram,ixugram2] <- 0
               }
 #            }
           }
@@ -196,7 +215,10 @@ conttable_construct <- function(date, epoch1, ngramlen2, epoch2=NULL, ngramlen1=
 #debug(countCooccurNooccurUnigram)
       unigGrp <- ddply(eg, c("unigram"), countCooccurNooccurUnigram)
     
-      res <- data.frame(egRow, uniqueUnigams=I(list(uniqueUgrams)), 
+      #Grand Total
+      cooccurs[ixTOTAL,ixTOTAL] <- sum(cooccurs[ixTOTAL,])
+      
+      res <- data.frame(egRow, uniqueUnigams=I(list(uniqueUgrams)), uniqueNgrams=I(list(eg[,"ngram"])), #unique from ngram table population
           unigramCooccurs=I(list(cooccurs)), unigramsNotoccurs=I(list(notoccurs)))
       if(DEBUG){
         str(res)
@@ -209,7 +231,8 @@ conttable_construct <- function(date, epoch1, ngramlen2, epoch2=NULL, ngramlen1=
 #setBreakpoint("concattable_construct.R#69")
 
     epochGrps <- ddply(df, c("epochstartux"), createCooccurNooccur)
-    epochGrps <- align_epochs(epochGrps,epoch1)
+    if(alignEpochs)
+      epochGrps <- align_epochs(epochGrps,epoch1)
   }
 
   #cleanup
