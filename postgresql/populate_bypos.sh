e74247bf32f7472c1f60dbd1acccc30fc248b2ac
Younos <yabouln@uwaterloo.ca>
2013-02-08 22:24:50 -0500

    By pos my buddy

--- postgresql/populate_bypos.sh
+++ postgresql/populate_bypos.sh
@@ -9,11 +9,11 @@ psql="psql -p 5433 -d ${db} -c "
 #echo "${psql} 'DROP TABLE IF EXISTS compgrams CASCADE;'"
 #${psql} 'CREATE UNLOGGED TABLE bypos() INHERITS(ngrams);'
 #(id int8, timeMillis int8, date int4, ngram text, ngramLen int2, tweetLen int2, pos int2);'"
-for p in 0 10 20 30 40 50 60 70 
+for p in 6 7 8 9 16 17 18 19 26 27 28 29 36 37 38 39 46 47 48 49 56 57 58 59 66 67 68 69
 do
-screen ${psql} "CREATE UNLOGGED TABLE unigramsP${p} (CHECK (pos = ${p})) INHERITS(bypos); \
+${psql} "CREATE UNLOGGED TABLE unigramsP${p} (CHECK (pos = ${p})) INHERITS(bypos); \
     COPY unigramsP${p} FROM '${root}bypos_onefile/unigramsP${p}';   \
-    CREATE INDEX unigramsP${p}_date ON unigramsP${p}(date);"
+    CREATE INDEX unigramsP${p}_date ON unigramsP${p}(date);"&
 #    CREATE INDEX ngrams${len}_ngramLen ON ngrams${len}(ngramLen);\""
 done
 
