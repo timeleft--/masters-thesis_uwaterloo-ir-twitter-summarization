69a191aff0b593e17718fa0a4ba817a7869cbf8a
yaboulna <yaboulna@uwaterloo.ca>
2013-02-10 14:45:52 -0500

    Load the unigrams and the compgrams.. not compgrams of different lengths

--- twitterr/compgrams_plot-vs-unigrams_stationary.R
+++ twitterr/compgrams_plot-vs-unigrams_stationary.R
@@ -2,10 +2,11 @@ G.plotsRoot <- "~/plots_r/"
 
 logLabelCPU <- "compgrams_plot-vs-unigrams"
 
-DEBUG_CPU <- TRUE
+DEBUG_CPU <- FALSE
 
 CPU.epoch2 <- '1hr'
 CPU.ngramlen2 <- 2
+CPU.supp <- 5
 
 if(DEBUG_CPU){
   G.dbCPU<-'full' #'sample-0.01'
@@ -13,10 +14,11 @@ if(DEBUG_CPU){
   CPU.nCores <- 2
   day <- 121106 
   db <-G.dbCPU 
+  support <- CPU.supp
 } else {
   G.dbCPU<-'full'
   CPU.days <- c(121110, 130103, 121016, 121206, 121210, 120925, 121223, 121205, 130104, 121108, 121214, 121030, 120930, 121123, 121125, 121027, 121105, 121116, 121106, 121222, 121026, 121028, 120926, 121008, 121104, 121103, 121122, 121114, 121231, 120914, 121120, 121119, 121029, 121215, 121013, 121220, 121212, 121111, 121217, 130101, 121226, 121127, 121128, 121124, 121229, 121020, 120913, 121121, 121007, 121010, 121203, 121207, 121218, 130102, 121025, 120920, 120929, 121009, 121126, 121021, 121002, 121201, 120918, 120919, 120927, 121012, 120924, 120928, 121024, 121209, 121115, 121112, 121227, 121101, 121113, 121211, 121204, 120921, 121224, 121130, 121208, 120922, 121230, 121001, 121006, 121031, 121015, 121129, 121014, 121003, 121117, 121118, 121213, 121107, 121109, 121004, 121019, 121022, 121017, 121023, 121216, 121225, 121102, 121202, 121018, 121005, 121011, 120917, 121221, 121228, 120923, 121219)
-  CPU.nCores <- 50
+  CPU.nCores <- 30
 }
 
 while(!require(foreach)){
@@ -39,7 +41,7 @@ while(!require(sm)){
   install.packages("sm")
 }
 
-plotDensitiesForDay <- function (day, epoch2=CPU.epoch2, ngramlen2=CPU.ngramlen2, db = G.dbCPU) {
+plotDensitiesForDay <- function (day, epoch1='1hr', ngramlen1=1, epoch2=CPU.epoch2, ngramlen2=CPU.ngramlen2, db = G.dbCPU, support=CPU.supp) {
  
   drv <- dbDriver("PostgreSQL")
   con <- dbConnect(drv, dbname=db, user="yaboulna", password="5#afraPG",
@@ -65,7 +67,7 @@ plotDensitiesForDay <- function (day, epoch2=CPU.epoch2, ngramlen2=CPU.ngramlen2
   
   ################
   
-  sql <- sprintf("SELECT epochstartux,ngramlen,cnt FROM %s", inTable)
+  sql <- sprintf("SELECT 2  as qnum, epochstartmillis/1000 as epochstartux,cnt FROM  cnt_%s%d where date=%d  and cnt > %d UNION ALL SELECT 1 as qnum, epochstartux,cnt FROM %s;", epoch1, ngramlen1, day, support, inTable)
   
   try(stop(paste(Sys.time(), logLabelCPU, " for day:", day, " - Fetching compgrams using sql: \n", sql)))
   
@@ -73,6 +75,7 @@ plotDensitiesForDay <- function (day, epoch2=CPU.epoch2, ngramlen2=CPU.ngramlen2
   
   compgramsDf <- fetch(compgramsRs,n=-1)
   
+  try(stop(paste(Sys.time(), logLabelCPU, " for day:", day, " - Fetched compgrams num rowsl: ", nrow(compgramsDf))))
   
   try(dbClearResult(compgramsRs))
   # dbDisconnect(con, ...) closes the connection. Eg.
@@ -83,35 +86,33 @@ plotDensitiesForDay <- function (day, epoch2=CPU.epoch2, ngramlen2=CPU.ngramlen2
   
   ###################
   
-  divideByngramLenAndPlot <- function(eg){
+  divideByQNumAndPlot <- function(eg){
    
-    #sm.density.compare does this for us
-#    compgramMask <- eg$ngramlen == ngramlen2
-#    compoundDf <- eg[which(compgramMask),"cnt"]
-#    unigramDf <- eg[which(!compgramMask),"cnt"]
-    
-    # function calls will result in copying of large amounts of data
-#    plotUnigramVsCompoundHistogram(compoundDf,unigramDf)
-    
     try(stop(paste(Sys.time(), logLabelCPU, " for day:", day, " - Will plot epoch", eg$epochstartux[1])))
   
     epochOut <- paste(outRoot, "/density_", epoch2,ngramlen2,'_',day,"-",eg$epochstartux[1],sep="")
     
     pdf(paste(epochOut,".pdf",sep=""))
    
-    equality <- sm.density.compare(eg$cnt, eg$ngramlen, xlab=paste("Number of occurrences per",epoch2), model="equal")
-    title(main=paste("Densities of unigrams and compgrams of length",ngramlen2,"\nin the",epoch2,"starting",eg$epochstartux[1]))
+    par(lwd=3)
+    equality <- sm.density.compare(eg$cnt, eg$qnum, 
+        xlab=paste("Number of occurrences per",epoch2),# I give up.. it stays cyan! col.band="gray", 
+        model="equal")
+
+    title(main=paste("Densities of unigrams and compgrams of length",ngramlen2,"\n in the",epoch2,"starting",eg$epochstartux[1]))
     
     # Add a legend (the color numbers start from 2 and go up)
-    legend("topright", levels(data$cond), fill=2+(0:nlevels(data$cond)))
+    legend("topright", legend=c(1,ngramlen2), fill=c(2,3))
    
     dev.off()
     
-    save(equality,ascii=TRUE, file=paste(epochOut,".equality",sep=""))
+    cat(paste("p:",equality$p,"\nupper:",paste(equality$upper,collapse=","),"\nlower:",paste(equality$lower,collapse=","),"\nh:",equality$h)
+    ,file=paste(epochOut,"_equality.txt",sep=""))
     try(stop(paste(Sys.time(), logLabelCPU, " for day:", day, " - Finished plotting epoch", eg$epochstartux[1])))
   }
+#  debug(divideByQNumAndPlot)
   
-  d_ply(idata.frame(compgramsDf), c("epochstartux"), divideByngramLenAndPlot)
+  d_ply(idata.frame(compgramsDf), c("epochstartux"), divideByQNumAndPlot)
   
   return(paste("Success for day", day))
 }
