1cd04837f2439a8c48d1d70690863d9083a070d0
yaboulna <yaboulna@uwaterloo.ca>
2013-02-11 17:54:36 -0500

    Ready to debug extending compgrams

--- twitterr/compgrams_extend.R
+++ twitterr/compgrams_extend.R
@@ -9,7 +9,7 @@ CGX.DEBUG <- TRUE
 
 CGX.eopch2 <- '1hr'
 CGX.ngramlen2 <- 2
-CGX.db <- 'full'
+CGX.support <- 5
 
 CGX.loglabel.DEFAULT <- "compgrams-extend"
 CGX.loglabel <- CGX.loglabel.DEFAULT
@@ -23,6 +23,11 @@ if(CGX.DEBUG){
   startPos=0
   inputPath = "~/r_output/compound_unigrams/"
   outputRoot = "~/r_output/"
+  CGX.days <- c(121110, 130103)
+  CGX.db <- 'full'
+} else {
+  CGX.days <- c(121110, 130103, 121016, 121206, 121210, 120925, 121223, 121205, 130104, 121108, 121214, 121030, 120930, 121123, 121125, 121027, 121105, 121116, 121106, 121222, 121026, 121028, 120926, 121008, 121104, 121103, 121122, 121114, 121231, 120914, 121120, 121119, 121029, 121215, 121013, 121220, 121212, 121111, 121217, 130101, 121226, 121127, 121128, 121124, 121229, 121020, 120913, 121121, 121007, 121010, 121203, 121207, 121218, 130102, 121025, 120920, 120929, 121009, 121126, 121021, 121002, 121201, 120918, 120919, 120927, 121012, 120924, 120928, 121024, 121209, 121115, 121112, 121227, 121101, 121113, 121211, 121204, 120921, 121224, 121130, 121208, 120922, 121230, 121001, 121006, 121031, 121015, 121129, 121014, 121003, 121117, 121118, 121213, 121107, 121109, 121004, 121019, 121022, 121017, 121023, 121216, 121225, 121102, 121202, 121018, 121005, 121011, 120917, 121221, 121228, 120923, 121219)
+  CGX.db <- 'full'
 }
 
 
@@ -97,7 +102,7 @@ extendCompgramOfDay <- function(day, epoch2=CGX.epoch2, ngramlen2=CGX.ngramlen2,
         fileEncoding = "UTF-8")
     
   sqlTemplate <- sprintf("SELECT id,ngram as unigram from unigramsp%%d where date=%d and id in (%s);",day,
-     paste(cgOcc$id,collapse=","))
+     paste(cgOcc$id,collapse=",")) # and cnt > %d, support
   
 #  ugDfCache <- data.frame(pos=c(startPos:maxPos))
       
@@ -172,3 +177,28 @@ extendCompgramOfDay <- function(day, epoch2=CGX.epoch2, ngramlen2=CGX.ngramlen2,
   
   return(paste("Success for day:"),day)
 }
+debug(extendCompgramOfDay)
+
+###############################
+### Driver
+##############################
+
+nullCombine <- function(a,b) NULL
+foreach(day=G.days,
+        .inorder=FALSE, .combine='nullCombine') %dopar%
+    {
+      daySuccess <- paste("Success for day", day) #"Unkown result for day",day)
+      
+      tryCatch({
+            
+            daySuccess <<- extendCompgramOfDay(day, 
+                epoch2 = CGX.epoch2, ngramlen2 = CGX.ngramlen2,  db = CGX.db, support = CGX.support)
+            
+          }
+          ,error=function(e) daySuccess <<- paste("Failure for day",day,e)
+          ,finally=try(stop(paste(Sys.time(), "ngram_assoc() for day:", day, " - ", daySuccess)))
+      )
+    }
+
+
+
