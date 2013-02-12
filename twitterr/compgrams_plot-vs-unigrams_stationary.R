a1b7e31120b93adc9db85e84701d5c8a16a020eb
yaboulna <yaboulna@uwaterloo.ca>
2013-02-12 11:49:41 -0500

    With rug, and without debug

--- twitterr/compgrams_plot-vs-unigrams_stationary.R
+++ twitterr/compgrams_plot-vs-unigrams_stationary.R
@@ -2,7 +2,7 @@ G.plotsRoot <- "~/plots_r/"
 
 logLabelCPU <- "compgrams_plot-vs-unigrams"
 
-DEBUG_CPU <- TRUE
+DEBUG_CPU <- FALSE
 
 CPU.epoch2 <- '1hr'
 CPU.ngramlen2 <- 2
@@ -17,7 +17,7 @@ if(DEBUG_CPU){
   support <- CPU.supp
 } else {
   G.dbCPU<-'full'
-  CPU.days <- c(121110, 130103, 121016, 121206, 121210, 120925, 121223, 121205, 130104, 121108, 121214, 121030, 120930, 121123, 121125, 121027, 121105, 121116, 121106, 121222, 121026, 121028, 120926, 121008, 121104, 121103, 121122, 121114, 121231, 120914, 121120, 121119, 121029, 121215, 121013, 121220, 121212, 121111, 121217, 130101, 121226, 121127, 121128, 121124, 121229, 121020, 120913, 121121, 121007, 121010, 121203, 121207, 121218, 130102, 121025, 120920, 120929, 121009, 121126, 121021, 121002, 121201, 120918, 120919, 120927, 121012, 120924, 120928, 121024, 121209, 121115, 121112, 121227, 121101, 121113, 121211, 121204, 120921, 121224, 121130, 121208, 120922, 121230, 121001, 121006, 121031, 121015, 121129, 121014, 121003, 121117, 121118, 121213, 121107, 121109, 121004, 121019, 121022, 121017, 121023, 121216, 121225, 121102, 121202, 121018, 121005, 121011, 120917, 121221, 121228, 120923, 121219)
+  CPU.days <- c(121110, 130103, 121016, 121206, 121210, 120925, 121223, 121205,121106) #, 130104, 121108, 121214, 121030, 120930, 121123, 121125, 121027, 121105, 121116,  121222, 121026, 121028, 120926, 121008, 121104, 121103, 121122, 121114, 121231, 120914, 121120, 121119, 121029, 121215, 121013, 121220, 121212, 121111, 121217, 130101, 121226, 121127, 121128, 121124, 121229, 121020, 120913, 121121, 121007, 121010, 121203, 121207, 121218, 130102, 121025, 120920, 120929, 121009, 121126, 121021, 121002, 121201, 120918, 120919, 120927, 121012, 120924, 120928, 121024, 121209, 121115, 121112, 121227, 121101, 121113, 121211, 121204, 120921, 121224, 121130, 121208, 120922, 121230, 121001, 121006, 121031, 121015, 121129, 121014, 121003, 121117, 121118, 121213, 121107, 121109, 121004, 121019, 121022, 121017, 121023, 121216, 121225, 121102, 121202, 121018, 121005, 121011, 120917, 121221, 121228, 120923, 121219)
   CPU.nCores <- 30
 }
 
@@ -119,8 +119,8 @@ plotDensitiesForDay <- function (day, epoch1=NULL, ngramlen1=1, epoch2=CPU.epoch
     par(mfrow=c(2,1))
     
     pdf(paste(epochOut,".pdf",sep=""))
-    
-    hist(log(ngramsDf[ngramEpochMask,"cnt"]), occBreaks, freq=FALSE, 
+    logNgrams <- log(ngramsDf[ngramEpochMask,"cnt"])
+    hist(logNgrams, occBreaks, freq=FALSE, 
         axes=FALSE, xlab="LOG Occurrences less than or equal",
         main=paste("Density of Ngrams of length",ngramlen1,"in each occurrences per",epoch1,"bin.\nN",
             paste(names(summaryNgrams),collapse="\t"),"\n",length(ngramEpochMask),paste(summaryNgrams,collapse="\t")))
@@ -128,18 +128,22 @@ plotDensitiesForDay <- function (day, epoch1=NULL, ngramlen1=1, epoch2=CPU.epoch
     axis(1,c(0,occBreaks))
     abline(h=ygrid,col="gray", lty=3)
 
+    rug(logNgrams)
+    
     legend("topright",legend=c("log(mean)","log(median)"),fill=c("blue","green"))
     abline(v=log(summaryNgrams['Mean']),col="blue")
     abline(v=log(summaryNgrams['Median']),col="green")
     
-    
-    hist(log(eg$cnt), occBreaks, freq=FALSE, 
+    logCompgrams <- log(eg$cnt)
+    hist(logCompgrams, occBreaks, freq=FALSE, 
         axes=FALSE,xlab="LOG Occurrences less than or equal",
         main=paste("Density of Compgrams of length",ngramlen2,"in each occurrences per",epoch2,"bin.\nN",
             paste(names(summaryNgrams),collapse="\t"),"\n",length(eg$cnt),paste(summaryCompgrams,collapse="\t")))
     axis(2,c(0,ygrid))
     axis(1,c(0,occBreaks))
     abline(h=ygrid,col="gray", lty=3)
+    
+    rug(logCompgrams)
     
     legend("topright",legend=c("log(mean)","log(median)"),fill=c("blue","green"))
     abline(v=log(summaryCompgrams['Mean']),col="blue")
