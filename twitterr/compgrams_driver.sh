600fafa7c077e2d39b308f186f8bec4fc084cae4
yaboulna <yaboulna@uwaterloo.ca>
2013-02-19 14:14:42 -0500

    I'll just run it on the full thing.. go go go

--- twitterr/compgrams_driver.sh
+++ twitterr/compgrams_driver.sh
@@ -5,9 +5,9 @@ exit 1
 fi
 
 runTS=${1}
-db=sample-0.01
+db=full
 
-for ngramlen1 in 2 3 4 5 6 7 8 9 10
+for ngramlen1 in 1 #2 3 4 5 6 7 8 9 10
 do
     ngramlen2=`expr ${ngramlen1} + 1`
     echo "\
