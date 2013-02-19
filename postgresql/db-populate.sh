d7ec9916513ad0a84c9358f3155769923205e3d0
yaboulna <yaboulna@uwaterloo.ca>
2013-02-19 12:44:09 -0500

    popoluate ugramsp

--- postgresql/db-populate.sh
+++ postgresql/db-populate.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# First install the conrib package
+#CREATE EXTENSION dblink
+# select * from dblink('dbname=full port=5433', 'select * from unigramsp0') as ugp0(id int8, timemillis int8, date int4, ngram text, ngramlen int2, tweetlen int2, pos int2);
+
 if [ $# -ne 2 ]; then
 echo "usage"
 exit 1
@@ -9,11 +13,12 @@ psql="psql -p 5433 -d ${db} -c "
 #echo "${psql} 'DROP TABLE IF EXISTS compgrams CASCADE;'"
 #${psql} 'CREATE UNLOGGED TABLE bypos() INHERITS(ngrams);'
 #(id int8, timeMillis int8, date int4, ngram text, ngramLen int2, tweetLen int2, pos int2);'"
-for p in 0 10 20 30 40 50 60 70 
+
+for p in {0..70} 
 do
-screen ${psql} "CREATE UNLOGGED TABLE unigramsP${p} (CHECK (pos = ${p})) INHERITS(bypos); \
+${psql} "CREATE UNLOGGED TABLE unigramsP${p} (CHECK (pos = ${p})) INHERITS(bypos); \
     COPY unigramsP${p} FROM '${root}bypos_onefile/unigramsP${p}';   \
-    CREATE INDEX unigramsP${p}_date ON unigramsP${p}(date);"
+    CREATE INDEX unigramsP${p}_date ON unigramsP${p}(date);"&
 #    CREATE INDEX ngrams${len}_ngramLen ON ngrams${len}(ngramLen);\""
 done
 
