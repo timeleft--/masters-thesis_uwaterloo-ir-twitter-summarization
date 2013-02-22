13ed7de2dc6e40556ddb1c6e7733aa3a73051425
Younos <yabouln@uwaterloo.ca>
2013-02-21 22:05:10 -0500

    rm deeh matesta7'demhash.. mv bass

--- postgresql/primer_wait.sh
+++ postgresql/primer_wait.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+echo 'for job in `jobs -p` 
+do 
+echo $job 
+wait $job 
+done' 
 
-tail -f /dev/null &
-tail -f /dev/null &
-tail -f /dev/null &
-jobs -p
+#tail -f /dev/null &
+#tail -f /dev/null &
+#tail -f /dev/null &
+#jobs -p
 
 for job in `jobs -p`
 do
--- postgresql/sel_fix_braces.sh
+++ postgresql/sel_fix_braces.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+root=${1}
 for nobraces in sel_121008.csv sel_121013.csv sel_121016.csv sel_121026.csv sel_121027.csv sel_121028.csv sel_121029.csv sel_121030.csv sel_121103.csv sel_121104.csv sel_121105.csv sel_121106.csv sel_121108.csv sel_121110.csv sel_121116.csv sel_121119.csv sel_121120.csv sel_121122.csv sel_121123.csv sel_121125.csv sel_121205.csv sel_121206.csv sel_121210.csv sel_121214.csv sel_121215.csv sel_121231.csv sel_130103.csv sel_130104.csv
 do
 
-cut -d "  " -f 1 ${root}/${nobraces} | nl -s "{" | cut -c7- > ${root}/${nobraces}_fix1.tmp
-cut -d "  " -f 2- ${root}/${nobraces} | nl -s "  " | cut -c7- > ${root}/${nobraces}_fix2.tmp
+cut -d '	' -f 1 ${root}/${nobraces} | nl -s '{' | cut -c7- > ${root}/${nobraces}_fix1.tmp
+cut -d '	' -f 2- ${root}/${nobraces} | nl -s '	' | cut -c7- > ${root}/${nobraces}_fix2.tmp
 
-paste -d "}" ${root}/${nobraces}_fix1.tmp ${root}/${nobraces}_fix2.tmp > ${root}/${nobraces}
+paste -d '}' ${root}/${nobraces}_fix1.tmp ${root}/${nobraces}_fix2.tmp > ${root}/${nobraces}
 
 done
 
