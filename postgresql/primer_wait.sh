fb07057ff0f5f2d6d08f8b9a24f1cb0155b25b41
yaboulna <yaboulna@uwaterloo.ca>
2013-02-22 09:46:37 -0500

    r shelataha leeeeh

--- postgresql/primer_wait.sh
+++ postgresql/primer_wait.sh
@@ -8,7 +8,7 @@ echo "${psql} 'select * from ngrams1 a join ngrams2 b on a.id=b.id;'&"
 echo "${psql} 'select * from ngrams2;'&"
 echo "${psql} 'select * from ngrams1;'&"
 
-running="-n \$(ps -U postgres  | grep -e \"${db} \[local\] \")"
+running="-n \$(ps r -U postgres  | grep -e \"${db} \[local\] \")"
 echo "while : ; do "
 echo " while [[ ${running} ]]; do sleep 5; done "
 echo " sleep 3; "
