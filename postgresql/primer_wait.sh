f7ff0e992e7790d93b7e8686ea4cba12c65f3477
yaboulna <yaboulna@uwaterloo.ca>
2013-02-22 09:39:49 -0500

    background process in test

--- postgresql/primer_wait.sh
+++ postgresql/primer_wait.sh
@@ -4,9 +4,9 @@ db=${1}
 
 psql="psql -p 5433 -d ${db} -c "
 
-echo "${psql} 'select * from ngrams1 a join ngrams2 b on a.id=b.id;'"
-echo "${psql} 'select * from ngrams2;'"
-echo "${psql} 'select * from ngrams1;'"
+echo "${psql} 'select * from ngrams1 a join ngrams2 b on a.id=b.id;'&"
+echo "${psql} 'select * from ngrams2;'&"
+echo "${psql} 'select * from ngrams1;'&"
 
 running="-n \$(ps -U postgres  | grep -e \"${db} \[local\] \")"
 echo "while : ; do "
