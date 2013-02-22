3125b5890c1d47458c9a61651440e837cd380176
yaboulna <yaboulna@uwaterloo.ca>
2013-02-22 09:23:25 -0500

    test wait

--- postgresql/primer_wait.sh
+++ postgresql/primer_wait.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
 db=${1}
+
+psql="psql -p 5433 -d ${db} -c "
+
+echo "${psql} 'select * from ngrams1 a join ngrams2 b on a.id=b.id;'"
+echo "${psql} 'select * from ngrams2;'"
+echo "${psql} 'select * from ngrams1;'"
+
 running="-n \$(ps -U postgres  | grep -e \"${db} \[local\] \")"
 echo "while : ; do "
 echo " while [[ ${running} ]]; do sleep 5; done "
@@ -7,27 +15,3 @@ echo " sleep 3; "
 echo " if [[ ! ${running} ]]; then echo 'DONE'; break; fi "
 echo "done "
  
-
-
-echo 'for job in `jobs -p` 
-do 
-echo $job 
-wait $job 
-done' 
-
-#tail -f /dev/null &
-#tail -f /dev/null &
-#tail -f /dev/null &
-#jobs -p
-
-for job in `jobs -p`
-do
-echo $job
-wait $job 
-done
-
-YOU CANNOT WAIT FOR A PROCESS THAT IS NOT A DIRECT CHILD OF YOUR PROCESS.. so db commands are out of the question:
-echo "while [[ -n \$(ps r -U postgres  | grep -e \"${db} \[local\] \") ]]; do sleep 0.1; done "
-
-OOPs.. the above might exit prematurely.. I guess it is tough luck that it tests at a moment when there is nothing running
-USE below instead??
