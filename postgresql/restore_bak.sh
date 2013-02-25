2b0bb35994706fd91d047c0227f80545a0dea7bb
yaboulna <yaboulna@uwaterloo.ca>
2013-02-24 20:54:09 -0500

    debug

--- postgresql/restore_bak.sh
+++ postgresql/restore_bak.sh
@@ -5,6 +5,6 @@ bak=`ls -1 ${root} | grep .*csv_.*bak`
 for b in ${bak}
 do
 n=`echo "${b}" | cut -d '_' -f 1`
-mv ${n} ${n}_reverted_${ts}
-mv ${b} ${n}
+mv ${root}/${n} ${root}/${n}_reverted_${ts}
+mv ${root}/${b} ${root}/${n}
 done
