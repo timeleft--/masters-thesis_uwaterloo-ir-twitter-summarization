2c75221f66545627e98e1ec5cd6cc0e694b07020
yaboulna <yaboulna@uwaterloo.ca>
2013-02-25 15:59:29 -0500

    omal kont be te3mel commit le 2eeh?

---
+++ twitterr/fix_sel-occ.R
@@ -0,0 +1,74 @@
+# TODO: Add comment
+# 
+# Author: yia
+###############################################################################
+FSO.ngramlen2=2
+FSO.days <- c(120925,130104)
+FSO.root <- paste("~/r_output/occ_yuleq_full_",FSO.ngramlen2,"/",sep="")
+#day<-130104
+source("compgrams_utils.R")
+require(plyr)
+FSO.db<-"full"
+
+for(day in FSO.days){
+  
+  alloccFile <- paste(FSO.root,day,".csv",sep="")
+  seloccFile <-  paste(FSO.root,"sel_",day,".csv",sep="")
+  try(file.rename(seloccFile,paste(seloccFile,"_fix_sel-occ_",format(Sys.time(),format="%y%m%d%H%M%S"),".bak",sep="") ))
+  selStaging <- paste(seloccFile,"staging",sep=".")
+  file.create(selStaging)
+  
+#  allOcc <- read.table(alloccFile, header = FALSE, quote = "", comment.char="", 
+#      sep = "\t", na = "NA", dec = ".", row.names = NULL, fill=TRUE,
+#      col.names = c("id","timemillis","date","ngram","ngramlen","tweetlen","pos"), #,"yq","dl","cnt"),
+#      colClasses = c("character","numeric","integer","character","integer","integer","integer"), #,"NULL","NULL","NULL"),
+#      fileEncoding = "UTF-8-MAC")
+  require(RPostgreSQL)
+
+  drv <- dbDriver("PostgreSQL")
+  con <- dbConnect(drv, dbname=FSO.db, user="yaboulna", password="5#afraPG",
+      host="hops.cs.uwaterloo.ca", port="5433")
+  
+  allOcc <- dbSendQuery(con,"select * from debug_allocc2_130104")
+      
+  
+#  docLenById <- array(allOcc$tweetlen, row.names=allOcc$id)
+  docLenById <- array(allOcc$tweetlen)
+  rownames(docLenById) <- allOcc$id
+  occupiedEnv <- initOccupiedEnv(docLenById)
+  rm(docLenById)
+  
+  sec0CurrDay <-  as.numeric(as.POSIXct(strptime(paste(day,"0000",sep=""),
+              "%y%m%d%H%M", tz="Pacific/Honolulu"),origin="1970-01-01")) 
+  epochCuts <- sec0CurrDay + (c(0:24) * 3600)  
+  allOcc$epoch <- cut(allOcc$timemillis/1000,breaks=epochCuts)
+  
+  fixEpoch <- function(epochOccs){
+    uniqueNgrams <- unique(epochOccs$ngram)
+    
+    for(ngram in uniqueNgrams){
+      ngramOccs <- epochOccs[which(epochOccs$ngram == ngram),]
+      
+      selOccs <- adply(ngramOccs,1,
+          selectOccurrences,ngramlen2 = FSO.ngramlen2,occupiedEnv = occupiedEnv,allowOverlap = FALSE,
+          colsToReturn=c("ngram","id","timemillis","date","ngramlen","tweetlen","pos"),
+          .expand=F)
+      selOccs$X1 <- NULL
+      
+      
+      write.table(selOccs, file = selStaging, append = TRUE, quote = FALSE, sep = "\t",
+          eol = "\n", na = "NA", dec = ".", row.names = FALSE,
+          col.names = FALSE, # qmethod = c("escape", "double"),
+          fileEncoding = "UTF-8")
+    }
+  }
+# debug(fixEpoch)
+  
+  d_ply(allOcc,c("epoch"),fixEpoch)
+  
+  file.rename(selStaging,seloccFile)
+  
+  rm(occupiedEnv)
+  
+  
+}
