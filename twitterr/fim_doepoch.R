98053185c9bbe9a8ecd487a77e10734750f254c1
yaboulna <yaboulna@scspc400.cs.uwaterloo.ca>
2013-02-26 23:37:37 -0500

    matenshash el add

---
+++ twitterr/fim_doepoch.R
@@ -0,0 +1,61 @@
+FIME.epochstartux<-FIME.compgramOccs$epochstartux[1]
+
+try(stop(paste(Sys.time(),FIM.label, " - FIM for epoch:",FIME.epochstartux, "num occs before pruning:",nrow(FIME.compgramOccs))))
+
+if(FIM.PRUNE_HIGHER_THAN_OBAMA){
+  
+  ########Read the compgram vocabulary
+  #day alread set
+  source("fim_less-than-obama.R", local = TRUE, echo = TRUE)
+  #FLO.compgramsDf should appear in the current environment after sourcing
+  
+  # Note that FLO.compgramDf is for one epoch only
+  FIME.midFreq <- merge(FIME.compgramOccs,FLO.compgramsDf,by="compgram",sort=F, suffixes=c("","FLO"))
+  
+#      cntFLO <- array(FLO.compgramsDf$cnt)
+#      names(cntFLO) <-FLO.compgramsDf$compgram
+#      
+#      FIME.midFreq <- a_ply(FIME.compgramOccs,1,function(occ) {
+#            if(is.na(cntFLO[occ$compgram])) 
+#                return(NULL)
+#            else
+#               return(occ)
+#            } ,.expand = FALSE)
+#  rm(FIME.compgramOccs)
+  rm(FLO.compgramsDf)
+} else {
+  # renaming will cost us a copy in case we don't want to do anything, right? NO:
+  # From http://cran.r-project.org/doc/manuals/R-ints.html
+  # The named field is set and accessed by the SET_NAMED and NAMED macros, and take values 0, 1 and 2. R has a ‘call by value’ illusion, so an assignment like
+  #     b <- a
+  #appears to make a copy of a and refer to it as b. However, if neither a nor b are subsequently altered there is no need to copy. What really happens is that a new symbol b is bound to the same value as a and the named field on the value object is set (in this case to 2). When an object is about to be altered, the named field is consulted. A value of 2 means that the object must be duplicated before being changed. (Note that this does not say that it is necessary to duplicate, only that it should be duplicated whether necessary or not.)
+  FIME.midFreq <- FIME.compgramOccs
+}
+
+try(stop(paste(Sys.time(),FIM.label, " - FIM for epoch:",FIME.epochstartux, "num occs after pruning:",nrow(FIME.midFreq))))
+
+# trans4 <- as(split(a_df3[,"item"], a_df3[,"TID"]), "transactions") 
+FIME.transacts <- as(split(FIME.midFreq$compgram, FIME.midFreq$id), "transactions")
+
+rm(FIME.midFreq)
+
+try(stop(paste(Sys.time(),FIM.label, " - num transactions:",length(FIME.transacts))))
+
+FIMW.epochFIS <- eclat(FIME.transacts,parameter = list(supp = FIM.support/length(FIME.transacts),minlen=2, maxlen=FIM.fislenm))
+
+rm(FIME.transacts)
+
+epochFile<-paste(FIME.outDir,"/fis_",FIME.epochstartux,".csv",sep="")
+write(FIMW.epochFIS,file=epochFile,sep="\t",
+    col.names=NA) #TODO: colnames
+
+try(stop(paste(Sys.time(),FIM.label, " - Interest for epoch:", FIME.epochstartux, "num FIS:",length(FIMW.epochFIS))))
+
+interest=interestMeasure(FIMW.epochFIS, c("lift","allConfidence","crossSupportRatio"),transactions = FIME.transacts)
+quality(FIMW.epochFIS) <- cbind(quality(FIMW.epochFIS), interest)
+#  # inspect(head(sort(dayFIS,by="crossSupportRatio")))
+rm(interest)
+
+write(FIMW.epochFIS,file=epochFile,sep="\t",
+    col.names=NA) #TODO: colnames
+try(stop(paste(Sys.time(),FIM.label, " - Done for epoch:", FIME.epochstartux, "file:",epochFile)))
