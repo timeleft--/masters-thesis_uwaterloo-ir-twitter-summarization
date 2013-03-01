abe887e87c6a869134f06005fe9eb78fcf2b4d53
yaboulna <yaboulna@scspc400.cs.uwaterloo.ca>
2013-03-01 16:12:38 -0500

    Select occurrences of candidates having more share of the stream than "Obama"

---
+++ twitterr/yaboulna_utils.R
@@ -0,0 +1,23 @@
+annotPrint <- function(label,...){
+  print(paste(Sys.time(), label, ..., sep=" | "))
+} 
+
+
+createOutFile <- function(outDir, outFile){
+  
+  if(!file.exists(outDir)){
+    dir.create(outDir,recursive = T)
+  }
+  
+  if(file.exists(outFile)){
+    bakname <- paste(outFile,"_",format(Sys.time(),format="%y%m%d%H%M%S"),".bak",sep="")
+    warning(paste("Renaming existing output file",outFile,bakname))
+    file.rename(outFile,bakname)
+  }
+  
+  retVal <- paste(outFile,"staging",sep=".")
+  
+  file.create(retVal)
+  
+  return(retVal)
+}
