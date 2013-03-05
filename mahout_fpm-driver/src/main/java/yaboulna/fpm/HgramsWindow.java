8211d1d47aaa2888d9d799b79b11d7856219a784
yaboulna <yaboulna@uwaterloo.ca>
2013-03-05 01:29:47 -0500

    log4j

--- mahout_fpm-driver/src/main/java/yaboulna/fpm/HgramsWindow.java
+++ mahout_fpm-driver/src/main/java/yaboulna/fpm/HgramsWindow.java
@@ -28,63 +28,72 @@ import com.google.common.collect.Lists;
 import com.google.common.io.Closeables;
 
 public class HgramsWindow {
- 
+
   protected static final DateTimeFormatter dateFmt = DateTimeFormat.forPattern("yyMMdd");
-  
- 
-  public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException{
-    
+  private static final boolean REMOVE_OUTPUT_AUTOMATICALLY = true;
+
+  public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
+
     long windowStartUx = Long.parseLong(args[0]);
-    DateMidnight startDay = new DateMidnight(windowStartUx, DateTimeZone.forID("HST"));
-    
+    DateMidnight startDay = new DateMidnight(windowStartUx*1000, DateTimeZone.forID("HST"));
+
     long windowEndUx = Long.parseLong(args[1]);
-    DateMidnight endDay = new DateMidnight(windowEndUx, DateTimeZone.forID("HST"));
-    
+    DateMidnight endDay = new DateMidnight(windowEndUx * 1000, DateTimeZone.forID("HST"));
+
     List<String> days = Lists.newLinkedList();
     MutableDateTime currDay = new MutableDateTime(startDay);
-    while(!currDay.isAfter(endDay)){
+    while (!currDay.isAfter(endDay)) {
       days.add(dateFmt.print(currDay));
       currDay.addDays(1);
     }
-    
+
     Path path = new Path(args[2]);
     Configuration conf = new Configuration();
     FileSystem fs = FileSystem.get(path.toUri(), conf);
-    
-    if(fs.exists(path)){
-      throw new IllegalArgumentException("Output path already exists.. remove it yourself: " + path.toUri());
+
+    if (fs.exists(path)) {
+      if (REMOVE_OUTPUT_AUTOMATICALLY) {
+        fs.delete(path, true);
+      } else {
+        throw new IllegalArgumentException("Output path already exists.. remove it yourself: "
+            + path.toUri());
+      }
     }
-    
-    SequenceFile.Writer writer = new SequenceFile.Writer(fs, conf, path, Text.class, TopKStringPatterns.class);
-    
+
+    SequenceFile.Writer writer = new SequenceFile.Writer(fs, conf, path, Text.class,
+        TopKStringPatterns.class);
+
     int minSupport = 5;
-    if(args.length > 3){
+    if (args.length > 3) {
       minSupport = Integer.parseInt(args[3]);
     }
-        
-    HgramTransactionIterator transIter = new HgramTransactionIterator(days, windowStartUx, windowEndUx, 2);
-    HgramTransactionIterator transIter2 = new HgramTransactionIterator(days, windowStartUx, windowEndUx, 2);
-    
-    try{
-    transIter.init();
-    transIter2.init();
-    
-    FPGrowth<String> fp = new FPGrowth<String>();
-    Set<String> features = new HashSet<String>();
-    fp.generateTopKFrequentPatterns(
-        transIter,
-        fp.generateFList(transIter2, minSupport),
-           minSupport,
-           days.size() * 120, //5 per hour
-           features,
-           new StringOutputConverter(new SequenceFileOutputCollector<Text, TopKStringPatterns>(writer)),
-           new ContextStatusUpdater(null));
-    
-    }finally{
+
+    HgramTransactionIterator transIter = new HgramTransactionIterator(days, windowStartUx,
+        windowEndUx, 2);
+    HgramTransactionIterator transIter2 = new HgramTransactionIterator(days, windowStartUx,
+        windowEndUx, 2);
+
+    try {
+      transIter.init();
+      transIter2.init();
+
+      FPGrowth<String> fp = new FPGrowth<String>();
+      Set<String> features = new HashSet<String>();
+      fp.generateTopKFrequentPatterns(
+          transIter,
+          fp.generateFList(transIter2, minSupport),
+          minSupport,
+          days.size() * 120, // 5 per hour
+          features,
+          new StringOutputConverter(new SequenceFileOutputCollector<Text, TopKStringPatterns>(
+              writer)),
+          new ContextStatusUpdater(null));
+
+    } finally {
       Closeables.closeQuietly(writer);
       transIter.uninit();
       transIter2.uninit();
     }
   }
-   
+
 }
