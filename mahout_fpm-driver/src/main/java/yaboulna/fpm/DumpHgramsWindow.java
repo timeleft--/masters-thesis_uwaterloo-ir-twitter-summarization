5d059730b1711a8ff57218fa6bcc9dcec6031408
yaboulna <yaboulna@scspc400.cs.uwaterloo.ca>
2013-03-07 15:15:23 -0500

    Better dump, selective

--- mahout_fpm-driver/src/main/java/yaboulna/fpm/DumpHgramsWindow.java
+++ mahout_fpm-driver/src/main/java/yaboulna/fpm/DumpHgramsWindow.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.mahout.common.Pair;
@@ -13,26 +14,45 @@ import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
 public class DumpHgramsWindow {
-  
-  public static String fpsToString(List<Pair<List<String>,Long>> frequentPatterns) {
-    StringBuilder sb = new StringBuilder();
+
+  public static String fpsToString(Pair<String, TopKStringPatterns> tokenPatterns) {
+    StringBuilder sb = new StringBuilder(tokenPatterns.getFirst()+"\t");
     String sep = " | ";
-    
-    for (Pair<List<String>,Long> pattern : frequentPatterns) {
+
+    boolean worthy = false;
+    for (Pair<List<String>, Long> pattern : tokenPatterns.getSecond().getPatterns()) {
+      if(pattern.getFirst().size() == 1){
+        continue;
+      }
+      worthy = true;
       sb.append(pattern.getFirst() + " = " + pattern.getSecond());
       sb.append(sep);
     }
-    
-    return sb.toString();
+
+    return (worthy?sb.toString():null);
   }
-  
+
   public static void main(String[] args) throws IOException {
     Path path = new Path(args[0]);
     Configuration conf = new Configuration();
     FileSystem fs = FileSystem.get(path.toUri(), conf);
-    List<Pair<String, TopKStringPatterns>> txt = FPGrowth.readFrequentPattern(conf, path);
-    for(Pair<String, TopKStringPatterns> t: txt){
-      System.out.println(t.getFirst() + "\t" + fpsToString(t.getSecond().getPatterns()));
+    for (FileStatus s : fs.listStatus(path)) {
+      if (s.isDir()) {
+        System.out.println("Skipping dir" + s.getPath());
+        continue;
+      }
+      System.out.println("Dumping (" + s.getLen() + " bytes): " + s.getPath());
+      try {
+        List<Pair<String, TopKStringPatterns>> txt = FPGrowth.readFrequentPattern(conf, s.getPath());
+        for (Pair<String, TopKStringPatterns> t : txt) {
+          String toPrint = fpsToString(t);
+          if(toPrint != null){
+            System.out.println(toPrint);
+          }
+        }
+      } catch (Exception ignored) {
+        System.err.println(ignored.getMessage());
+      }
     }
   }
 }
