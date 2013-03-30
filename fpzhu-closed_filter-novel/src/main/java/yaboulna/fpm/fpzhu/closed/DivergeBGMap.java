899da41f7642bcda843dc1d61aa3746fa855dc36
yaboulna <yaboulna@scspc400.cs.uwaterloo.ca>
2013-03-30 11:32:28 -0400

    KL Divergence's brother

--- fpzhu-closed_filter-novel/src/main/java/yaboulna/fpm/fpzhu/closed/DivergeBGMap.java
+++ fpzhu-closed_filter-novel/src/main/java/yaboulna/fpm/fpzhu/closed/DivergeBGMap.java
@@ -2,31 +2,40 @@ package yaboulna.fpm.fpzhu.closed;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.Writer;
-import java.nio.channels.Channels;
 import java.util.Collections;
+import java.util.Formatter;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.comparator.NameFileComparator;
 import org.apache.commons.io.filefilter.FileFilterUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Charsets;
 import com.google.common.base.Splitter;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableMap.Builder;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Maps;
 import com.google.common.io.Closer;
 import com.google.common.io.Files;
 import com.google.common.io.LineProcessor;
+import com.google.common.primitives.Doubles;
 
 public class DivergeBGMap {
+  private final static Logger LOG = LoggerFactory.getLogger(DivergeBGMap.class);
 
   static class ItemsetTabCountProcessor implements LineProcessor<Map<String, Integer>> {
 
     public static final String NUM_TWEETS_KEY = "NUMTWEETS";
-    Builder<String, Integer> mapBuilder = ImmutableMap.builder();
+
+// Builder<String, Integer> mapBuilder = ImmutableMap.builder();
+    final Map<String, Integer> fpCntMap;
+// Avoid copying this from one frame to another = Maps.newHashMapWithExpectedSize(4444444);
+
+    public ItemsetTabCountProcessor(Map<String, Integer> fpCntMap) {
+      this.fpCntMap = fpCntMap;
+    }
 
     @Override
     public boolean processLine(String line) throws IOException {
@@ -38,16 +47,23 @@ public class DivergeBGMap {
 
       String itemset = (tabIx > 0 ? line.substring(0, tabIx) : NUM_TWEETS_KEY);
 
-      mapBuilder.put(itemset, count);
+// mapBuilder.put(itemset, count);
+      fpCntMap.put(itemset, count);
 
       return true;
     }
 
     @Override
     public Map<String, Integer> getResult() {
-      return mapBuilder.build();
+      return null; // fpCntMap avoid unneccesary copying.. am I thinking in C or R?? whatever!
     }
   }
+
+  // max num of itemsets was 4434143 according to wc -l of the folder lcm_closed/4wk+1wk...1-abs672
+  private static final int BG_MAX_NUM_ITEMSETS = 4444444;
+
+  // max num of itemsets was 2688780 fp_3600_1352260800 in the folder lcm_closed/1hr+30min...1-abs5
+  private static final int FG_MAX_NUM_ITEMSETS = 2700000;
   /**
    * @param args
    * @throws IOException
@@ -68,12 +84,13 @@ public class DivergeBGMap {
     List<File> fgFiles = (List<File>) FileUtils.listFiles(fgDir, FileFilterUtils.prefixFileFilter("fp_"),
         FileFilterUtils.trueFileFilter());
     Collections.sort(fgFiles, NameFileComparator.NAME_COMPARATOR);
+    Map<String, Integer> fgMap = Maps.newHashMapWithExpectedSize(FG_MAX_NUM_ITEMSETS);
 
     List<File> bgFiles = (List<File>) FileUtils.listFiles(bgDir, FileFilterUtils.prefixFileFilter("fp_"),
         FileFilterUtils.trueFileFilter());
     Collections.sort(bgFiles, NameFileComparator.NAME_COMPARATOR);
     long loadedBgStartUx = -1;
-    Map<String, Integer> bgMap = null;
+    Map<String, Integer> bgMap = Maps.newHashMapWithExpectedSize(BG_MAX_NUM_ITEMSETS);;
 
     Splitter underscoreSplit = Splitter.on('_');
 
@@ -86,21 +103,28 @@ public class DivergeBGMap {
         long bgFileWinStart = Long.parseLong(Iterables.get(underscoreSplit.split(bgFiles.get(b).getName()), 2));
         long nextBgFileWinStart = Long.MAX_VALUE;
         if (b < bgFiles.size() - 1) {
-          Long.parseLong(Iterables.get(underscoreSplit.split(bgFiles.get(b + 1).getName()), 2));
+          nextBgFileWinStart = Long.parseLong(Iterables.get(underscoreSplit.split(bgFiles.get(b + 1).getName()), 2));
         }
         if (b == bgFiles.size() - 1 || (idealBgStartUx >= bgFileWinStart && idealBgStartUx < nextBgFileWinStart)) {
           if (loadedBgStartUx != bgFileWinStart) {
-
-            bgMap = Files.readLines(bgFiles.get(b), Charsets.UTF_8, new ItemsetTabCountProcessor());
+            LOG.info("Loading background freqs from {}", bgFiles.get(b));
+            loadedBgStartUx = bgFileWinStart;
+            bgMap.clear();
+            Files.readLines(bgFiles.get(b), Charsets.UTF_8, new ItemsetTabCountProcessor(bgMap));
+            LOG.info("Loaded background freqs - num itemsets: {} ", bgMap.size());
           }
           break;
         }
       }
 
-      Map<String, Integer> fgMap = Files.readLines(fgF, Charsets.UTF_8, new ItemsetTabCountProcessor());
+      fgMap.clear();
+      LOG.info("Loading foreground freqs from {}", fgF);
+      Files.readLines(fgF, Charsets.UTF_8, new ItemsetTabCountProcessor(fgMap));
+      LOG.info("Loaded foreground freqs - num itemsets: {}", fgMap.size());
 
       final double bgNumTweets = bgMap.get(ItemsetTabCountProcessor.NUM_TWEETS_KEY);
       final double fgNumTweets = fgMap.get(ItemsetTabCountProcessor.NUM_TWEETS_KEY);
+      final double bgFgLogP = Math.log((bgNumTweets + fgMap.size())/(fgNumTweets+ fgMap.size()));
 
       final File novelFile = new File(fgF.getParentFile(), fgF.getName().replaceFirst("fp_", "novel_"));
       if (novelFile.exists()) {
@@ -109,22 +133,30 @@ public class DivergeBGMap {
 
       Closer novelClose = Closer.create();
       try {
-        final Writer novelWr = novelClose.register(Channels.newWriter(FileUtils.openOutputStream(novelFile)
-            .getChannel(), Charsets.UTF_8.name()));
+        Formatter highPrecFormat = novelClose.register(new Formatter(novelFile,Charsets.UTF_8.name()));
+// final Writer novelWr = novelClose.register(Channels.newWriter(FileUtils.openOutputStream(novelFile)
+// .getChannel(), Charsets.UTF_8.name()));
 
+        int counter = 0;
         for (String itemset : fgMap.keySet()) {
-          double fgLogP = Math.log(fgMap.get(itemset) / fgNumTweets);
+          double fgFreq = fgMap.get(itemset) + 1;
+//          double fgLogP = Math.log(fgFreq / fgNumTweets);
 
-          double bgLogP;
+          
           Integer bgCount = bgMap.get(itemset);
-          if (bgCount == null) {
-            bgLogP = 2 * fgLogP; // so that the final result will be the abs(fpLogP)
-          } else {
-            bgLogP = Math.log(bgCount / bgNumTweets);
-          }
+          if (bgCount == null) 
+            bgCount = 1;
+//            bgLogP = 2 * fgLogP; // so that the final result will be the abs(fpLogP)
+//          } else {
+//            bgLogP = Math.log(bgCount / bgNumTweets);
+//          }
 
-          double logOdds = fgLogP - bgLogP;
-          novelWr.append(itemset).append("\t" + logOdds);
+          double klDiver = fgFreq * (Math.log(fgFreq / bgCount) + bgFgLogP);
+//          novelWr.append(itemset).append('\t').append(Doubles. String.format(klDiver)).append('\n');
+          highPrecFormat.format(itemset + "\t%.15f\n", klDiver);
+          if (++counter % 10000 == 0) {
+            LOG.info("Processed {} itemsets. Last one: {}", counter, itemset + "=" + klDiver);
+          }
         }
       } finally {
         novelClose.close();
