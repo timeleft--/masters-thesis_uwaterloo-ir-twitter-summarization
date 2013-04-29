c985d7e50f8e309bf28495c169e827773fbc138a
Younos Aboulnaga <yaboulna@uwaterloo.ca>
2013-04-29 16:59:41 -0400

    more useful information

--- diff_itemset-collections/src/main/java/yaboulna/fpm/Diff.java
+++ diff_itemset-collections/src/main/java/yaboulna/fpm/Diff.java
@@ -13,6 +13,7 @@ import java.util.regex.Pattern;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.apache.commons.io.filefilter.IOFileFilter;
+import org.apache.commons.lang.mutable.MutableInt;
 
 import yaboulna.fpm.postgresql.PerfMonKeyValueStore;
 
@@ -24,32 +25,32 @@ import com.google.common.io.Files;
 import com.google.common.io.LineProcessor;
 
 public class Diff {
-//  private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(Diff.class);
+// private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(Diff.class);
 
-  static class MyAffixFileFilter implements IOFileFilter{
+  static class MyAffixFileFilter implements IOFileFilter {
 
-  final String afx;
-  final boolean prefix;
-  
-  public MyAffixFileFilter(String afx, boolean isPrefix) {
-    super();
-    this.afx = afx;
-    this.prefix = isPrefix;
-  }
+    final String afx;
+    final boolean prefix;
 
-  public boolean accept(File file) {
-    return accept(file.getParentFile(), file.getName());
-  }
+    public MyAffixFileFilter(String afx, boolean isPrefix) {
+      super();
+      this.afx = afx;
+      this.prefix = isPrefix;
+    }
 
-  public boolean accept(File dir, String name) {
-    return (prefix? name.startsWith(afx) : name.endsWith(afx));
-  }
-    
+    public boolean accept(File file) {
+      return accept(file.getParentFile(), file.getName());
+    }
+
+    public boolean accept(File dir, String name) {
+      return (prefix ? name.startsWith(afx) : name.endsWith(afx));
+    }
+
   }
   static abstract class AbstractLineProcessor implements LineProcessor<Integer> {
-    
+
     Integer retval = 0;
-    
+
     Pattern tabSplitPattern = Pattern.compile("\\t");
 
     Pattern commaSplitPattern = Pattern.compile("\\,");
@@ -92,13 +93,13 @@ public class Diff {
 
     File diffDir = new File(dataDir, "diff");
     diffDir.mkdir();
-    
+
     final Set<String> keywords = Sets.newHashSet(Arrays.copyOfRange(args, 3, args.length));
 
     List<File> selFiles = (List<File>) FileUtils.listFiles(dataDir,
-        FileFilterUtils.and(new MyAffixFileFilter(selPfx, true), //FileFilterUtils.prefixFileFilter(selPfx),
+        FileFilterUtils.and(new MyAffixFileFilter(selPfx, true), // FileFilterUtils.prefixFileFilter(selPfx),
             FileFilterUtils.notFileFilter(new MyAffixFileFilter("diff", false)), // FileFilterUtils.prefixFileFilter("diff")),
-            FileFilterUtils.notFileFilter(new MyAffixFileFilter(".log", false))), //FileFilterUtils.suffixFileFilter("log"))),
+            FileFilterUtils.notFileFilter(new MyAffixFileFilter(".log", false))), // FileFilterUtils.suffixFileFilter("log"))),
         FileFilterUtils.trueFileFilter());
     Collections.sort(selFiles);
 
@@ -111,21 +112,22 @@ public class Diff {
 
       File origFile = new File(selF.getParentFile(), replaceFirst(selF.getName(), selPfx, origPfx));
       if (!origFile.exists()) {
-//        LOG.debug("Orig file {} does not exist for selection file {}", origFile, selF);
+// LOG.debug("Orig file {} does not exist for selection file {}", origFile, selF);
         System.out.println("Orig file does not exist for selection file: " + selF);
         continue;
       }
 
       Closer diffClose = Closer.create();
       try {
-        File diffFile = new File(diffDir, replaceFirst(selF.getName(),selPfx,
-            "diff_" + Joiner.on("-").join((keywords.isEmpty() ? Arrays.asList("NO", "KEYWORDS") : keywords)) 
+        File diffFile = new File(diffDir, replaceFirst(selF.getName(), selPfx,
+            "diff_" + Joiner.on("-").join((keywords.isEmpty() ? Arrays.asList("NO", "KEYWORDS") : keywords))
                 + "_" + origPfx + "-" + selPfx));
-//        LOG.debug
-        
+// LOG.debug
+
         System.out.println(origFile + " - " + selF + " = " + diffFile);
-        
-        final PerfMonKeyValueStore perfmonKV = diffClose.register(new PerfMonKeyValueStore(Diff.class.getName(), diffFile.getAbsolutePath()));
+
+        final PerfMonKeyValueStore perfmonKV = diffClose.register(new PerfMonKeyValueStore(Diff.class.getName(),
+            diffFile.getAbsolutePath()));
         final Formatter diffFmt = diffClose.register(new Formatter(diffFile));
 
         Files.readLines(selF, Charsets.UTF_8, new AbstractLineProcessor() {
@@ -138,10 +140,7 @@ public class Diff {
 
         });
 
-        File selKeywordsFile = new File(diffFile.getParentFile(),  replaceFirst(selF.getName(),selPfx,
-            selPfx + Joiner.on("-").join((keywords.isEmpty() ? Arrays.asList("NO", "KEYWORDS") : keywords)))); 
-        FileUtils.writeLines(selKeywordsFile, selSet);
-        
+        final MutableInt origOccsOfKeyWords = new MutableInt(0);
         double leftOutCount = Files.readLines(origFile, Charsets.UTF_8, new AbstractLineProcessor() {
 
           @Override
@@ -149,6 +148,8 @@ public class Diff {
             if (!keywords.isEmpty() && Sets.intersection(keywords, itemset).isEmpty()) {
               return;
             }
+
+            origOccsOfKeyWords.increment();
 
             for (Set<String> selected : selSet) {
               if (Sets.intersection(selected, itemset).equals(itemset)) {
@@ -158,15 +159,23 @@ public class Diff {
 
             ++retval;
             diffFmt.format("%s\n", itemset.toString());
-//            LOG.info("{}", itemset);
+// LOG.info("{}", itemset);
           }
 
         });
 
-        perfmonKV.storeKeyValue("LeftOutCount", leftOutCount);
-        perfmonKV.storeKeyValue("NumSelItemsets", selSet.size());
-        perfmonKV.storeKeyValue("PctLeftOut", leftOutCount == 0? 0 : leftOutCount / (selSet.size() + leftOutCount));
-        
+        if (origOccsOfKeyWords.doubleValue() > 0) {
+          File selKeywordsFile = new File(diffFile.getParentFile(), replaceFirst(selF.getName(), selPfx,
+              selPfx + Joiner.on("-").join((keywords.isEmpty() ? Arrays.asList("NO", "KEYWORDS") : keywords))));
+          FileUtils.writeLines(selKeywordsFile, selSet);
+          perfmonKV.storeKeyValue("OrigOccsOfKWs", origOccsOfKeyWords.doubleValue());
+          perfmonKV.storeKeyValue("LeftOutCount", leftOutCount);
+          perfmonKV.storeKeyValue("NumSelItemsets", selSet.size());
+          perfmonKV
+              .storeKeyValue("LeftOutPct", leftOutCount == 0 ? 0 : leftOutCount / origOccsOfKeyWords.doubleValue());
+          perfmonKV.storeKeyValue("SelToOrigCompRatio", selSet.size()
+              / (origOccsOfKeyWords.doubleValue() - leftOutCount));
+        }
       } finally {
         diffClose.close();
       }
