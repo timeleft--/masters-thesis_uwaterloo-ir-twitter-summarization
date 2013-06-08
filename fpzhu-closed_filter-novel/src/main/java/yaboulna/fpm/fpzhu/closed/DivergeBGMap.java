bdb281b7d3d9aa17d65cf6476a988d93894bda7c
yaboulna <yaboulna@uwaterloo.ca>
2013-06-07 21:51:32 -0400

    graph of all nodes

--- fpzhu-closed_filter-novel/src/main/java/yaboulna/fpm/fpzhu/closed/DivergeBGMap.java
+++ fpzhu-closed_filter-novel/src/main/java/yaboulna/fpm/fpzhu/closed/DivergeBGMap.java
@@ -668,10 +668,10 @@ public class DivergeBGMap {
 
             if (GRAPH_FILE) {
               Element rootElt = new Element("gexf", gexNs);
-              rootElt.setNamespace(vizNs);
+              rootElt.addNamespaceDeclaration(vizNs);
               Namespace xsiNs = Namespace.getNamespace("xsi","http://www.w3.org/2001/XMLSchema-instance");
               rootElt.setNamespace(xsiNs);
-              rootElt.setAttribute("schemaLocation", "http://www.gexf.net/1.2draft/gexf.xsd", xsiNs);
+              rootElt.setAttribute("schemaLocation", "http://www.gexf.net/1.2draft http://www.gexf.net/1.2draft/gexf.xsd", xsiNs);
               rootElt.setAttribute("version","1.2");
               gexDoc = new Document(rootElt);
               gexGraph = new Element("graph", gexNs);
